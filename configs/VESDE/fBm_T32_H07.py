da8fe8e50188ad62f41f8cb8d642fb2cd591cc7e
marcostapiac <marcostapia4@gmail.com>
2023-11-18 15:28:15 +0000

    Synchronising VESDE experiment across remote servers

--- configs/VESDE/fBm_T32_H07.py
+++ configs/VESDE/fBm_T32_H07.py
@@ -1,3 +1,5 @@
+
+
 import ml_collections
 import numpy as np
 import torch
@@ -13,21 +15,21 @@ def get_config():
 
     # Data set parameters
     config.hurst = 0.7
-    config.timeDim = 256
+    config.timeDim = 32
     config.data_path = project_config.ROOT_DIR + "data/fBn_samples_H{}_T{}.npy".format(
         str(config.hurst).replace(".", ""), config.timeDim)
 
     # Training hyperparameters
     config.train_eps = 1e-5
-    config.max_diff_steps = 2*1000 * max(int(np.log2(config.timeDim) - 1), 1)
+    config.max_diff_steps = 4000 #* max(int(np.log2(config.timeDim) - 1), 1)
     config.end_diff_time = 1.
     config.save_freq = 50
     config.lr = 1e-3
-    config.max_epochs = 300
+    config.max_epochs = 60000
     config.batch_size = 256
 
     # Diffusion hyperparameters
-    config.std_max = 80.
+    config.std_max = 90.
     config.std_min = 0.01
 
     # MLP Architecture parameters
@@ -42,13 +44,13 @@ def get_config():
     config.dialation_length = 10
 
     # Model filepath
-    mlpFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_MLP_fBm_VESDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_StdMax{:.4e}_StdMin{:.4e}_TembDim{}_EncShapes{}".format(
+    mlpFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_MLP_fBm_VESDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_StdMax{:.4e}_StdMin{:.4e}_TembDim{}_EncShapes{}_60000".format(
         config.hurst,
         config.timeDim,
         config.max_diff_steps, config.end_diff_time, config.train_eps, config.std_max, config.std_min, config.temb_dim,
         config.enc_shapes).replace(".", "")
 
-    tsmFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_TSM_fBm_VESDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_StdMax{:.3e}_StdMin{:.4e}_DiffEmbSize{}_ResidualLayers{}_ResChan{}_DiffHiddenSize{}".format(
+    tsmFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_TSM_fBm_VESDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_StdMax{:.3e}_StdMin{:.4e}_DiffEmbSize{}_ResidualLayers{}_ResChan{}_DiffHiddenSize{}_60000".format(
         config.hurst,
         config.timeDim,
         config.max_diff_steps, config.end_diff_time, config.train_eps, config.std_max, config.std_min, config.temb_dim,
@@ -66,14 +68,14 @@ def get_config():
 
     # Sampling hyperparameters
     config.sample_eps = 1e-5
-    config.max_lang_steps = 1
+    config.max_lang_steps = 0
     config.snr = 0.01
     config.predictor_model = "ancestral"  # vs "euler-maryuama"
     config.corrector_model = "VE"  # vs "VE"
 
     # Experiment evaluation parameters
-    config.dataSize = 70000
-    config.num_runs = 5
+    config.dataSize = 100000
+    config.num_runs = 20
     config.unitInterval = True
     config.plot = False
     config.annot_heatmap = False
@@ -93,7 +95,7 @@ def get_config():
     config.test_pred_lstm = False
     config.test_disc_lstm = False
     config.lookback = 10
-    config.pred_lstm_max_epochs = 2000
+    config.pred_lstm_max_epochs = 700
     config.disc_lstm_max_epochs = 5000
     config.lstm_batch_size = 128
     config.disc_lstm_trained_path = config.scoreNet_trained_path.replace(
