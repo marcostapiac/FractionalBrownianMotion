6de1d6a0d5691ec21386e1d4d3e22986a0fe6f98
Marcos Tapia Costa <marcostapia4@gmail.com>
2023-11-20 13:07:34 +0000

    Initiative 4

--- src/generative_modelling/models/ClassVPSDEDiffusion.py
+++ src/generative_modelling/models/ClassVPSDEDiffusion.py
@@ -110,7 +110,7 @@ class VPSDEDiffusion(nn.Module):
             predicted_score = score_network.forward(x, t.squeeze(-1)).squeeze(1)
             max_diff_steps = torch.Tensor([max_diff_steps]).to(diff_index.device)
             drift = self.get_ancestral_drift(x=x, pred_score=predicted_score, diff_index=diff_index, max_diff_steps=max_diff_steps)
-            diff_param = self.get_ancestral_diffusion_param(diff_index=diff_index, max_diff_steps=max_diff_steps)
+            diff_param = self.get_ancestral_diff(diff_index=diff_index, max_diff_steps=max_diff_steps)
         return predicted_score, drift, diff_param
 
     def get_ancestral_drift(self, x:torch.Tensor, pred_score:torch.Tensor , diff_index: torch.Tensor, max_diff_steps: torch.Tensor)->torch.Tensor:
