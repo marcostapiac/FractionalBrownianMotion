2da34f854a2407905f3994225a5eb01be9d6dd54
Marcos Tapia Costa <marcostapia4@gmail.com>
2023-11-21 18:22:07 +0000

    Prepared for remote server multi gpu

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -7,40 +7,6 @@ from sklearn import datasets
 from torch.distributed import init_process_group, destroy_process_group
 
 from configs import project_config
-from src.classes.ClassFractionalCEV import FractionalCEV
-from utils.plotting_functions import plot_subplots
-
-
-def gen_and_store_statespace_data(Xs=None, Us=None, muU=1., muX=1., gamma=1., X0=1., U0=0., H=0.8, N=2 ** 11,
-                                  T=1e-3 * 2 ** 11) -> None:
-    """
-    Generate observation and latent signal from CEV model and store it as pickle file
-        :param Xs: Optional parameter, array containing latent signal process
-        :param Us: Optional parameter, array containing observation process
-        :param muU: Drift parameter for observation process
-        :param muX: Drift parameter for latent process
-        :param gamma: Mean reversion parameter for latent process
-        :param X0: Initial value for latent process
-        :param U0: Initial value for observation process
-        :param H: Hurst Index
-        :param N: Length of time series
-        :param T: Terminal simualtion time for a process on [0, T]
-        :return: None
-    """
-    assert ((Xs is None and Us is None) or (Xs is not None and Us is not None))
-    sigmaX = np.sqrt(muX * gamma / 0.55)
-    alpha = gamma / sigmaX
-    deltaT = T / N
-    if Xs is None:
-        m = FractionalCEV(muU=muU, muX=muX, sigmaX=sigmaX, alpha=alpha, X0=X0, U0=U0)
-        Xs, Us = m.euler_simulation(H=H, N=N, deltaT=deltaT)  # Simulated data
-    df = pd.DataFrame.from_dict(data={'Log-Price': Us, 'Volatility': Xs})
-    plot_subplots(np.arange(0, T + deltaT, step=deltaT), np.array([Xs, Us]), np.array([None, None]),
-                  np.array(["Time", "Time"]),
-                  np.array(["Volatility", "Log Price"]),
-                  "Project Model Simulation")
-    df.to_csv('../data/raw_data_simpleObsModel_{}_{}.csv'.format(int(np.log2(N)), int(10 * H)), index=False)
-
 
 def energy_csv_to_df() -> pd.DataFrame:
     """
