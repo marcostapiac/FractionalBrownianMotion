f00a8839886e8fe8148be3f42c3e32e1418a98a9
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-02-06 18:51:00 +0000

    Reproducibility

--- experiments/generative_modelling/RecursiveVPSDE/Reproducibility/recursive_fBm_VPSDE_T256_H07_experiment_repro.py
+++ experiments/generative_modelling/RecursiveVPSDE/Reproducibility/recursive_fBm_VPSDE_T256_H07_experiment_repro.py
@@ -1,17 +1,15 @@
-import pickle
-
 import numpy as np
 import pandas as pd
 import torch
 
-from src.generative_modelling.data_processing import train_and_save_recursive_diffusion_model, \
-    recursive_LSTM_reverse_sampling
+from src.generative_modelling.data_processing import recursive_LSTM_reverse_sampling
 from src.generative_modelling.models.ClassVPSDEDiffusion import VPSDEDiffusion
 from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditionalTimeSeriesScoreMatching import \
     ConditionalTimeSeriesScoreMatching
 from src.generative_modelling.models.TimeDependentScoreNetworks.ClassNaiveMLP import NaiveMLP
 from utils.data_processing import init_experiment, cleanup_experiment
-from utils.math_functions import generate_fBn
+
+
 def repro_weights_init(m):
     if isinstance(m, torch.nn.LSTM):
         for name, param in m.named_parameters():
