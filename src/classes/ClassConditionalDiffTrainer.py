401474f1b32d7ede043d554d8af87402421207d5
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-02-06 17:48:43 +0000

    Trying to aggregate lossess across GPUs and batches

--- src/classes/ClassConditionalDiffTrainer.py
+++ src/classes/ClassConditionalDiffTrainer.py
@@ -249,18 +249,22 @@ class ConditionalDiffusionModelTrainer:
             :return: None
         """
         self.score_network.train()
+        all_losses_per_gpu = []
         for epoch in range(self.epochs_run, max_epochs):
             t0 = time.time()
             gpu_epoch_losses = self._run_epoch(epoch)
             # Append epoch loss for each GPU
             self.loss_tracker.append(torch.mean(torch.tensor(gpu_epoch_losses)).item())
+            all_gpus_losses = torch.distributed.all_gather(torch.tensor(self.loss_tracker).cuda())
+            average_loss_per_gpu = torch.mean(all_gpus_losses, dim=0)
+            all_losses_per_gpu.append(average_loss_per_gpu.cpu().numpy())
             print("Device {}: Loss Tracker {}\n".format(self.device_id, self.loss_tracker))
             # NOTE: .compute() cannot be called on only one process since it will wait for other processes
             # see  https://github.com/Lightning-AI/torchmetrics/issues/626
             print("Device {} :: Percent Completed {:0.4f} :: Train {:0.4f} :: Time for One Epoch {:0.4f}\n".format(self.device_id, (epoch + 1) / max_epochs,
                                                                         float(
                                                                             self.loss_aggregator.compute().item()),float(time.time()-t0)))
-            print("Running Loss Averaged Across GPUs, Epochs, and Batches on Device {}: {}\n".format(self.device_id, torch.mean(torch.tensor(self.loss_tracker)).item()))
+            print("Running Loss Averaged Across GPUs, Epochs, and Batches on Device {}: {}\n".format(self.device_id, all_losses_per_gpu))
             if self.device_id == 0 or type(self.device_id) == torch.device:
                 if epoch + 1 == max_epochs:
                     self._save_model(filepath=model_filename, final_epoch=epoch+1)
