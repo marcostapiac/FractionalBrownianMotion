05a82702df1392a87824bd5c9a8f8be330f35022
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-03-01 21:28:13 +0000

    Early stopping

---
+++ experiments/generative_modelling/RecursiveVPSDE/LossTracking/recursive_Markovian_fBm_VPSDE_T256_H07_infOnly_tl5.py
@@ -0,0 +1,42 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+import torch
+
+from src.classes.ClassConditionalMarkovianDiffTrainer import ConditionalMarkovianDiffusionModelTrainer
+from src.generative_modelling.data_processing import recursive_LSTM_reverse_sampling, \
+    train_and_save_recursive_diffusion_model
+from src.generative_modelling.models.ClassVPSDEDiffusion import VPSDEDiffusion
+from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditionalMarkovianTimeSeriesScoreMatching import \
+    ConditionalMarkovianTimeSeriesScoreMatching
+from src.generative_modelling.models.TimeDependentScoreNetworks.ClassNaiveMLP import NaiveMLP
+from utils.data_processing import init_experiment, cleanup_experiment
+from utils.math_functions import generate_fBn
+
+if __name__ == "__main__":
+    # Data parameters
+    from configs.RecursiveVPSDE.recursive_Markovian_fBm_T256_H07_tl_5data import get_config
+
+    config = get_config()
+    assert (0 < config.hurst < 1.)
+    assert (config.early_stop_idx == 0)
+    assert(config.tdata_mult == 5)
+    print(config.scoreNet_trained_path, config.dataSize)
+    rng = np.random.default_rng()
+    scoreModel = ConditionalMarkovianTimeSeriesScoreMatching(*config.model_parameters) if config.model_choice == "TSM" else NaiveMLP(
+        *config.model_parameters)
+    diffusion = VPSDEDiffusion(beta_max=config.beta_max, beta_min=config.beta_min)
+
+    init_experiment(config=config)
+    end_epoch = 150
+    scoreModel.load_state_dict(torch.load(config.scoreNet_trained_path + "_NEp" + str(end_epoch)))
+    for train_epoch in config.max_epochs:
+        scoreModel.load_state_dict(torch.load(config.scoreNet_trained_path + "_NEp" + str(train_epoch)))
+        final_paths = recursive_LSTM_reverse_sampling(diffusion=diffusion, scoreModel=scoreModel,
+                                                      data_shape=(config.dataSize, config.ts_length, 1), config=config)
+        df = pd.DataFrame(final_paths)
+        df.index = pd.MultiIndex.from_product(
+            [["Final Time Samples"], [i for i in range(config.dataSize)]])
+        df.to_csv(config.experiment_path + "_NEp{}.csv.gzip".format(train_epoch), compression="gzip")
+    cleanup_experiment()
