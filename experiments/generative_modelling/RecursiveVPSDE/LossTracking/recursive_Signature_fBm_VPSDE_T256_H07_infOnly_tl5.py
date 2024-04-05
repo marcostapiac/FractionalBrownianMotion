a6fc9294813c62592c648d234fd863172e157499
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-04-05 15:26:04 +0100

    Bug fix for inference only

--- experiments/generative_modelling/RecursiveVPSDE/LossTracking/recursive_Signature_fBm_VPSDE_T256_H07_infOnly_tl5.py
+++ experiments/generative_modelling/RecursiveVPSDE/LossTracking/recursive_Signature_fBm_VPSDE_T256_H07_infOnly_tl5.py
@@ -9,6 +9,8 @@ from src.classes.ClassConditionalSignatureDiffTrainer import ConditionalSignatur
 from src.generative_modelling.data_processing import train_and_save_recursive_diffusion_model, \
     recursive_signature_reverse_sampling
 from src.generative_modelling.models.ClassVPSDEDiffusion import VPSDEDiffusion
+from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditionalSignatureTimeSeriesScoreMatching import \
+    ConditionalSignatureTimeSeriesScoreMatching
 from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditionalTimeSeriesScoreMatching import \
     ConditionalTimeSeriesScoreMatching
 from src.generative_modelling.models.TimeDependentScoreNetworks.ClassNaiveMLP import NaiveMLP
@@ -27,7 +29,7 @@ if __name__ == "__main__":
     assert (config.tdata_mult == 5)
     print(config.scoreNet_trained_path, config.dataSize)
     rng = np.random.default_rng()
-    scoreModel = ConditionalTimeSeriesScoreMatching(
+    scoreModel = ConditionalSignatureTimeSeriesScoreMatching(
         *config.model_parameters) if config.model_choice == "TSM" else NaiveMLP(
         *config.model_parameters)
     diffusion = VPSDEDiffusion(beta_max=config.beta_max, beta_min=config.beta_min)
