453acd372d7da6c3e97cef496597719f96bd3c33
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-04-08 20:45:09 +0100

    Changing feature computation in score network

--- src/classes/ClassConditionalSignatureDiffTrainer.py
+++ src/classes/ClassConditionalSignatureDiffTrainer.py
@@ -149,7 +149,7 @@ class ConditionalSignatureDiffusionModelTrainer(nn.Module):
                 self.score_network.eval()
                 with torch.no_grad():
                     features = self.score_network.module.signet.forward(batch[:,:-1,:],  time_ax=torch.atleast_2d((torch.arange(1, T) / T)).T, basepoint=True)
-                    ts_time = 2 # We have generated x1, x2 (1-indexed)
+                    ts_time = 3 # We have generated x1, x2 (1-indexed)
                     past_feat = features[[0],[1],:] # Feature using information from x1,x2 (1-indexed)
                     basepoint = torch.atleast_3d(batch[0,1,:]) # Feature for generating x_2 most recent information is x_(ts_time-1) (1-indexed)
                     latest_path = torch.atleast_3d(batch[0, 2,:]) # x3
