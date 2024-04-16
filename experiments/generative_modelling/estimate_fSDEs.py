a2c92271051203ea3fcf6122aee351c86d90881e
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-04-17 00:30:14 +0200

    Buf fix on hurst estimation argument

--- experiments/generative_modelling/estimate_fSDEs.py
+++ experiments/generative_modelling/estimate_fSDEs.py
@@ -1,3 +1,5 @@
+from gzip import BadGzipFile
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -26,37 +28,44 @@ def second_order_estimator(paths: np.ndarray, Nsamples: int):
     return U_a1, U_a2
 
 
-def estimate_hurst_from_filter(Ua1: np.ndarray, Ua2: np.ndarray):
+def estimate_hurst_from_filter(Ua1: np.ndarray, Ua2: np.ndarray, epoch: int):
     assert (Ua1.shape == Ua2.shape)
     hs = 0.5 * np.log2(Ua2 / Ua1)
     plot_histogram(hs, num_bins=150, xlabel="H", ylabel="density",
-                   plottitle="Histogram of {} {} samples' estimated Hurst parameter")
+                   plottitle="Histogram of {} samples' estimated Hurst parameter".format(epoch))
     plt.show()
     return hs
 
 
-def estimate_fSDEs(config: ConfigDict):
-    fOU = pd.read_csv("/Users/marcos/GitHubRepos/FractionalBrownianMotion/experiments/results/rec_TSM_False_incs_True_unitIntv_fOU_VPSDE_model_H7000e-01_T256_Ndiff10000_Tdiff1000e+00_trainEps1e-04_BetaMax20000e+01_BetaMin10000e-04_DiffEmbSize64_ResLay10_ResChan8_DiffHiddenSize64_TrueHybrid_TrueWghts_LSTM_H20_Nlay1_fOU10_tl5_NEp480.csv.gzip", compression="gzip", index_col=[0,1]).to_numpy()
+def estimate_fSDEs(config: ConfigDict, path:str,train_epoch: int):
+    try:
+        fOU = pd.read_csv(path, compression="gzip", index_col=[0, 1]).to_numpy()
+    except BadGzipFile as e:
+        fOU = pd.read_parquet(path, engine="pyarrow").to_numpy()
+
     # We want to construct first an estimator for H based on sample paths
-    for _ in range(10):
+    for _ in range(1000):
         idx = np.random.randint(0, fOU.shape[0])
-        plt.plot(np.linspace(0, 1, config.ts_length), fOU[idx,:])
+        plt.plot(np.linspace(0, 1, config.ts_length), fOU[idx, :])
     plt.show()
     plt.close()
     N, T = fOU.shape
     assert (config.hurst < 0.75)
     U_a1, U_a2 = simple_estimator(paths=fOU, Nsamples=N)
-    estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2)
+    estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2, epoch=train_epoch)
     U_a1, U_a2 = second_order_estimator(paths=fOU, Nsamples=N)
-    estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2)
+    hs = estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2, epoch=train_epoch)
+    return hs
+
+def estimate_fSDE_from_true(config: ConfigDict):
     fOU = np.load(config.data_path, allow_pickle=True)
     N, T = fOU.shape
     assert (config.hurst < 0.75)
     U_a1, U_a2 = simple_estimator(paths=fOU, Nsamples=N)
-    estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2)
+    estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2, epoch="True")
     U_a1, U_a2 = second_order_estimator(paths=fOU, Nsamples=N)
-    estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2)
-
+    hs = estimate_hurst_from_filter(Ua1=U_a1, Ua2=U_a2, epoch="True")
+    return hs
 
 def check_hurst_condition_estimator(H: float):
     assert (0 < H < 1)
@@ -64,10 +73,13 @@ def check_hurst_condition_estimator(H: float):
     l2 = (2 * H - 1) / (2. - 2 * H)
     assert (max(l1, l2) <= 1)
 
-
+"""
 if __name__ == "__main__":
     from configs.RecursiveVPSDE.recursive_fOU_T256_H07_tl_5data import get_config
 
     config = get_config()
     check_hurst_condition_estimator(H=config.hurst)
-    estimate_fSDEs(config=config)
+    for epoch in config.max_epochs:
+        estimate_fSDEs(config=config, train_epoch=epoch)
+    estimate_fSDE_from_true(config=config)
+"""
