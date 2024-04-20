597a302d74f014032771544b725004bc942d9530
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-04-20 14:13:58 +0100

    SAVING MEMORY

--- src/classes/ClassConditionalSDESampler.py
+++ src/classes/ClassConditionalSDESampler.py
@@ -37,6 +37,5 @@ class ConditionalSDESampler:
             x, pred_score, noise, mean_est, var_est = self.predictor.step(x, t=t, diff_index=diff_index, feature=feature)
             if type(self.corrector) != NoneType:
                 x = self.corrector.sample(x, pred_score, noise, diff_index, self.predictor.max_diff_steps)
-        print(var_est.shape,  mean_est.shape, var_est.device, mean_est.device, shape, torch_device)
-        assert(mean_est.shape == shape and var_est.shape == shape and (mean_est.device == var_est.device == torch_device))
+        assert(mean_est.shape == shape and var_est.shape == shape)
         return x, mean_est, var_est
