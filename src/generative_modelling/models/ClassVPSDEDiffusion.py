7fb338dbcac06d524868c3f86349aef8fbd2f43d
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-04-20 10:43:21 +0100

    SAVING MEMORY

--- src/generative_modelling/models/ClassVPSDEDiffusion.py
+++ src/generative_modelling/models/ClassVPSDEDiffusion.py
@@ -139,11 +139,13 @@ class VPSDEDiffusion(nn.Module):
                 - Ancestral Sampling Diffusion Coefficient
         """
         score_network.eval()
+        score_network.zero_grad()
         predicted_score = score_network.forward(x, conditioner=feature, times=t)
         max_diff_steps = torch.Tensor([max_diff_steps]).to(diff_index.device)
         drift = self.get_ancestral_drift(x=x, pred_score=predicted_score, diff_index=diff_index,
                                          max_diff_steps=max_diff_steps)
         diff_param = self.get_ancestral_diff(diff_index=diff_index, max_diff_steps=max_diff_steps)
+        score_network.zero_grad()
         return predicted_score, drift, diff_param
 
     def get_ancestral_drift(self, x: torch.Tensor, pred_score: torch.Tensor, diff_index: torch.Tensor,
