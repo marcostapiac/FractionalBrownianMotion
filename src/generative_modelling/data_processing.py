9de70130eea1b3643130d91da25fe26176118350
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-04-23 09:12:38 +0100

    MKV Sampling bug fix

--- src/generative_modelling/data_processing.py
+++ src/generative_modelling/data_processing.py
@@ -336,16 +336,16 @@ def recursive_markovian_reverse_sampling(diffusion: VPSDEDiffusion,
         else:
             if "fOU" in config.data_path:
                 past = [torch.zeros_like(paths[0]) for _ in range(max(0, config.mkv_blnk - t))] + paths
-                past = torch.stack(past, dim=2)
+                past = torch.stack(past, dim=1)
                 past = past.cumsum(dim=1)
-                assert(past.shape == (data_shape[0], max(1,t+1), config.ts_dims))
+                assert(past.shape == (data_shape[0], max(1,t), config.ts_dims,1))
                 features = past[:,-config.mkv_blnk:,:].reshape(
                     (data_shape[0], 1, config.mkv_blnk * config.ts_dims, 1)).squeeze(-1)
                 assert(features.shape == (data_shape[0], 1, config.mkv_blnk * config.ts_dims))
             else:
                 past = [torch.zeros_like(paths[0]) for _ in range(max(0, config.mkv_blnk - t))] + paths[
                                                                                                   -config.mkv_blnk:]
-                features = torch.stack(past, dim=2).reshape(
+                features = torch.stack(past, dim=1).reshape(
                     (data_shape[0], 1, config.mkv_blnk * config.ts_dims, 1)).squeeze(-1)
         samples, mean, var = sampler.sample(shape=(data_shape[0], data_shape[-1]), torch_device=device, feature=features,
                                  early_stop_idx=config.early_stop_idx)
