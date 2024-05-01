a4a00d6614b59035d9d5fc08020bf9b0e4031999
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-05-01 16:32:44 +0100

    Trailling low variance score

--- src/generative_modelling/data_processing.py
+++ src/generative_modelling/data_processing.py
@@ -355,6 +355,7 @@ def recursive_markovian_reverse_sampling(diffusion: VPSDEDiffusion,
     paths = []
     means = []
     vars = []
+    prev_path = torch.zeros(size=(data_shape[0], 1))
     for t in range(config.ts_length):
         print("Sampling at real time {}\n".format(t + 1))
         if t == 0:
@@ -376,11 +377,19 @@ def recursive_markovian_reverse_sampling(diffusion: VPSDEDiffusion,
         samples, mean, var = sampler.sample(shape=(data_shape[0], data_shape[-1]), torch_device=device, feature=features,
                                  early_stop_idx=config.early_stop_idx, ts_step=1./config.ts_length, param_time=config.param_time)
         # Samples are size (BatchSize, 1, TimeSeriesDimension)
+        if t != 0:
+            est_mean = mean * config.ts_length
+            assert (est_mean.shape == prev_path.shape)
+            print("Estimated drift {}\n".format(est_mean))
+            print("Expected drift {}\n".format(-config.mean_rev * prev_path))
+            print(torch.mean(est_mean), torch.std(est_mean), torch.mean(prev_path), torch.std(prev_path),
+                  torch.mean(-config.mean_rev * prev_path), torch.std(-config.mean_rev * prev_path))
+
         assert (samples.shape == (data_shape[0], 1, data_shape[-1]))
-        print(torch.mean(var),torch.std(var))
         paths.append(samples.detach())
         means.append(mean.detach())
         vars.append(var.detach())
+        prev_path = torch.concat(paths, dim=1).squeeze(dim=2).sum(axis=1).unsqueeze(-1)
     final_paths = np.atleast_2d(torch.squeeze(torch.concat(paths, dim=1).cpu(), dim=2).numpy())
     conditional_means = np.atleast_2d(torch.concat(means, dim=1).cpu().numpy())
     conditional_vars = np.atleast_2d(torch.concat(vars, dim=1).cpu().numpy())
