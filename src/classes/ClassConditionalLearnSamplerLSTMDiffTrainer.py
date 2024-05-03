7e305a2e5e8f15aaf9f73a3ebbefb3fe052aae02
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-05-03 10:22:50 +0100

    Fixing drift estimator

--- src/classes/ClassConditionalLearnSamplerLSTMDiffTrainer.py
+++ src/classes/ClassConditionalLearnSamplerLSTMDiffTrainer.py
@@ -121,9 +121,9 @@ class ConditionalLSTMSampleDiffusionModelTrainer(nn.Module):
         # For VPSDE only
         beta_tau = torch.exp(-0.5 * eff_times)
         sigma_tau = (1 - torch.exp(-eff_times))
-        scaled_outputs = (sigma_tau/beta_tau)*outputs+xts/beta_tau
+        scaled_outputs = (sigma_tau/beta_tau)*outputs+xts/beta_tau # This leaves the network ONLY
         # Outputs should be (NumBatches, TimeSeriesLength, 1)
-        scaled_target_scores = (sigma_tau/beta_tau)*target_scores +xts/beta_tau
+        scaled_target_scores = (sigma_tau/beta_tau)*target_scores +xts/beta_tau # This leaves the original data sample ONLY
         return self._batch_loss_compute(outputs=scaled_outputs, targets= scaled_target_scores)
 
     def _run_epoch(self, epoch: int) -> list:
