8ec8957f63c23252ebf940be2b37b376740c0e4c
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-05-06 23:49:11 +0100

    New training objective

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/recursive_fOU_VPSDE_T256_H07_PostMean_tl5.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/recursive_fOU_VPSDE_T256_H07_PostMean_tl5.py
@@ -29,13 +29,13 @@ if __name__ == "__main__":
         *config.model_parameters) if config.model_choice == "TSM" else NaiveMLP(
         *config.model_parameters)
     diffusion = VPSDEDiffusion(beta_max=config.beta_max, beta_min=config.beta_min)
-    #init_experiment(config=config)
+    init_experiment(config=config)
     end_epoch = max(config.max_epochs)
     try:
         scoreModel.load_state_dict(torch.load(config.scoreNet_trained_path + "_NEp" + str(end_epoch)))
     except FileNotFoundError as e:
         print("Error {}; no valid trained model found; proceeding to training\n".format(e))
-        training_size = 1000+0*int(
+        training_size = int(
             min(config.tdata_mult * sum(p.numel() for p in scoreModel.parameters() if p.requires_grad), 1200000))
         print(training_size)
         try:
