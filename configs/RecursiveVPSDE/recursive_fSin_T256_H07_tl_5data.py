70bb3098176fd4c0d5671febf4bf7db85304d2fb
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-05-20 10:21:39 +0100

    Bug fix in PMSin

--- configs/RecursiveVPSDE/recursive_fSin_T256_H07_tl_5data.py
+++ configs/RecursiveVPSDE/recursive_fSin_T256_H07_tl_5data.py
@@ -92,7 +92,7 @@ def get_config():
     config.snr = 0.
     config.predictor_model = "CondAncestral"
     config.corrector_model = "VP"  # vs "VE" vs "OUSDE"
-    config.param_time = 900
+    config.param_time = 100
 
     # Experiment evaluation parameters
     config.dataSize = 40000
