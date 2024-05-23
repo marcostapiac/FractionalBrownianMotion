d24849b4e50f127328d8a6b4322b4b3f0957b602
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-05-23 10:33:54 +0100

    Pushing chnages to score/drfit eval

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/PM_single_past_drift_eval.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/PM_single_past_drift_eval.py
@@ -1,4 +1,4 @@
-
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
@@ -16,11 +16,11 @@ from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditional
 config_postmean = get_config_postmean()
 
 rng = np.random.default_rng()
-N = 2000
+N = 2
 data_shape = (N, 1, 1)
-device = "cuda:0"
+device = "cpu"
 
-diff_time_scale = torch.linspace(start=config_postmean.end_diff_time, end=config_postmean.sample_eps,
+diff_time_space = torch.linspace(start=config_postmean.end_diff_time, end=config_postmean.sample_eps,
                                  steps=config_postmean.max_diff_steps).to(device)
 real_time_scale = torch.linspace(start=1 / config_postmean.ts_length, end=1, steps=config_postmean.ts_length).to(device)
 diffusion = VPSDEDiffusion(beta_max=config_postmean.beta_max, beta_min=config_postmean.beta_min)
@@ -39,7 +39,10 @@ PM_960.load_state_dict(torch.load(config_postmean.scoreNet_trained_path + "_NEp"
 true_paths = np.load(config_postmean.data_path, allow_pickle=True)[:N,:]
 true_paths = true_paths.reshape((true_paths.shape[0], true_paths.shape[1], 1))
 true_paths = np.concatenate([np.zeros((true_paths.shape[0],1,true_paths.shape[-1])), true_paths], axis=1)
-drifts = []
+start_time_idx = 200
+end_time_idx = 256
+end_diff_idx = 9100
+drifts = np.zeros(shape=(end_time_idx-start_time_idx, max_diff_steps-end_diff_idx))
 path_values = []
 PM_960.eval()
 with torch.no_grad():
@@ -49,15 +52,16 @@ with torch.no_grad():
     # Fix a single past feature for a single time
     del tensor_true_paths, output
 
-for i in range(200,256):
-    path_values.append(true_paths[[0], [i],:].flatten()[0])
-    fixed_feature = torch.tile(features[[0], [i], :].unsqueeze(0),dims=(N, 1,1))
+for i in tqdm(range(start_time_idx,end_time_idx)):
+    pathidx = np.random.randint(low=0, high=true_paths.shape[0])
+    path_values.append(true_paths[[pathidx], [i],:].flatten()[0])
+    fixed_feature = torch.tile(features[[pathidx], [i], :].unsqueeze(0),dims=(N, 1,1))
     # Now run reverse-diffusion
     with torch.no_grad():
         diff_time_space = torch.linspace(start=config_postmean.end_diff_time, end=config_postmean.sample_eps,
                                         steps=config_postmean.max_diff_steps).to(device)
         x = diffusion.prior_sampling(shape=data_shape).to(device)  # Move to correct device
-        for diff_index in tqdm(range(0, config_postmean.max_diff_steps - 9100)):
+        for diff_index in (range(0, config_postmean.max_diff_steps - end_diff_idx)):
             tau = diff_time_space[diff_index] * torch.ones((data_shape[0],)).to(device)
             tau = tau * torch.ones((x.shape[0],)).to(device)
             eff_times = diffusion.get_eff_times(diff_times=tau)
@@ -71,24 +75,25 @@ for i in range(200,256):
                                                                                 max_diff_steps=config_postmean.max_diff_steps)
             #if len(score.shape) == 3 and score.shape[-1] == 1:
             #    score = score.squeeze(-1)
-            if diff_index == 900-1:
-                # Evaluate the drifts means
-                diffusion_mean2 = torch.atleast_2d(torch.exp(-diffusion.get_eff_times(diff_times=tau[0]))).T.to(device).squeeze()
-                diffusion_mean = torch.atleast_2d(torch.exp(-0.5*diffusion.get_eff_times(diff_times=tau[0]))).T.to(device).squeeze()
-                diffusion_var = 1. - diffusion_mean2
-                drift_est = ((diffusion_var + diffusion_mean2*ts_step)/(diffusion_mean*ts_step))*score + x/(diffusion_mean*ts_step)
+            # Evaluate the drifts means
+            diffusion_mean2 = torch.atleast_2d(torch.exp(-diffusion.get_eff_times(diff_times=tau[0]))).T.detach().cpu().squeeze()
+            diffusion_mean = torch.atleast_2d(torch.exp(-0.5*diffusion.get_eff_times(diff_times=tau[0]))).T.detach().cpu().squeeze()
+            diffusion_var = 1. - diffusion_mean2
+            drift_est = ((diffusion_var + diffusion_mean2*ts_step)/(diffusion_mean*ts_step))*score.detach().cpu() + x.detach().cpu()/(diffusion_mean*ts_step)
+            drifts[i-start_time_idx,diff_index] = torch.mean(drift_est).flatten().detach().cpu()[0]
             z = torch.randn_like(drift)
             x = drift + diffParam * z
-    drifts.append(torch.mean(drift_est).flatten().detach().cpu()[0])
-
-paired = zip(path_values, drifts)
-sorted_pairs = sorted(paired, key=lambda x: x[0])
+assert(len(path_values) == drifts.shape[0])
+sorted_idxs = np.argsort(path_values)
+path_values = np.array(path_values)[sorted_idxs]
+drifts = drifts[sorted_idxs, :]
 # Separate the pairs back into two arrays
-currprevpath, currmeanest = zip(*sorted_pairs)
-currprevpath = np.array(currprevpath)
-currmeanest = np.array(currmeanest)
-plt.plot(currprevpath, currmeanest, label="Estimated")
-plt.plot(currprevpath, -config_postmean.mean_rev*currprevpath, label="Expected")
+ax = plt.axes(projection='3d')
+x, y = np.meshgrid(diff_time_space[:config_postmean.max_diff_steps-end_diff_idx], path_values) # x is the columns, y is the rows
+ax.scatter3D(x, y, drifts, cmap='viridis', label="Estimated")
+ax.scatter3D(x, y,  -config_postmean.mean_rev*path_values.reshape((path_values.shape[0],1))*np.ones((drifts.shape[0], drifts.shape[1])), label="Expected")
 plt.legend()
+ax.set_xlabel('Diffusion Time', rotation=150)
+ax.set_ylabel('Path/State Value',  rotation=150)
 plt.show()
 plt.close()
