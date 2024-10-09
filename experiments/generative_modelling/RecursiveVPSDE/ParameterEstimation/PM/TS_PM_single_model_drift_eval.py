bf776947217af04a6e2fe5cfe0f88b4d98dfb8ad
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-10-09 21:24:27 +0200

    New file for non PM

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/TS_PM_single_model_drift_eval.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/TS_PM_single_model_drift_eval.py
@@ -164,7 +164,7 @@ ts_step = 1 / config_postmean.ts_length
 Nepoch = config_postmean.max_epochs[1]
 print(Nepoch)
 print(config_postmean.scoreNet_trained_path)
-save_path = project_config.ROOT_DIR + f"experiments/results/DriftEvalExp_{Nepoch}Nep_{config_postmean.loss_factor}LFactor"
+save_path = project_config.ROOT_DIR + f"experiments/results/TSPM_DriftEvalExp_{Nepoch}Nep_{config_postmean.loss_factor}LFactor"
 # Fix the number of training epochs and training loss objective loss
 PM = ConditionalLSTMTSScoreMatching(*config_postmean.model_parameters).to(device)
 PM.load_state_dict(torch.load(config_postmean.scoreNet_trained_path + "_NEp" + str(Nepoch)))
