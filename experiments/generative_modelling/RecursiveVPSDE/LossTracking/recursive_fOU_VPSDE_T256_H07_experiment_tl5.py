b9c9a2442143fa3728f0179cea5dc5a7736c4190
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-10-30 22:33:19 +0100

    Training non PM fSin

--- experiments/generative_modelling/RecursiveVPSDE/LossTracking/recursive_fOU_VPSDE_T256_H07_experiment_tl5.py
+++ experiments/generative_modelling/RecursiveVPSDE/LossTracking/recursive_fOU_VPSDE_T256_H07_experiment_tl5.py
@@ -18,7 +18,7 @@ from utils.math_functions import generate_fOU
 
 if __name__ == "__main__":
     # Data parameters
-    from configs.RecursiveVPSDE.recursive_fOU_T256_H05_tl_5data import get_config
+    from configs.RecursiveVPSDE.recursive_fSin_T256_H05_tl_5data import get_config
 
     config = get_config()
     assert (0 < config.hurst < 1.)
