8fe7b049737c36081ad9639102a7804df6302231
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-11-06 12:56:02 +0000

    Adding training stops

--- configs/RecursiveVPSDE/recursive_PostMeanScaledScore_fSin_T256_H05_tl_5data.py
+++ configs/RecursiveVPSDE/recursive_PostMeanScaledScore_fSin_T256_H05_tl_5data.py
@@ -26,7 +26,7 @@ def get_config():
     config.end_diff_time = 1.
     config.save_freq = 50
     config.lr = 1e-3
-    config.max_epochs = [12920]
+    config.max_epochs = [960, 1440, 1920, 2920, 6920, 12920]
     config.batch_size = 256
     config.isfBm = True
     config.isUnitInterval = True
