95136c92fa11d977f498bd3e9ce9a28ec4acde1c
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-01-07 23:07:10 +0100

    Reucing memory overload for benchmark

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/benchmarks.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/benchmarks.py
@@ -23,7 +23,7 @@ mean_rev = 1.
 
 # In[4]:
 
-num_paths = 2000
+num_paths = 1000
 ddata = np.load(project_config.ROOT_DIR + "data/fSin_samples_H05_T256_10Rev_10Diff_00Init.npy")
 idxs = np.arange(ddata.shape[0])
 path_observations = ddata[:num_paths,:]
