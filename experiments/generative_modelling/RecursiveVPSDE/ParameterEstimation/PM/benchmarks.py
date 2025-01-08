a8452c83edfe5c7358ecfc14193034e348e2af5d
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-01-08 12:05:12 +0100

    Reucing memory overload for benchmark

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/benchmarks.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/benchmarks.py
@@ -12,7 +12,7 @@ import tracemalloc
 
 
 def gaussian_kernel(bw, x):
-    return norm.pdf(x / bw) / bw
+    return np.exp(-0.5 * (x / bw) ** 2) / (bw * ((2 * np.pi) ** 0.5))  ##
 
 
 def memory_check(str):
@@ -32,7 +32,7 @@ Delta = 1. / ts_length
 mean_rev = 1.
 
 # In[4]:
-tracemalloc.start()
+# tracemalloc.start()
 num_paths = 10000
 ddata = np.load(project_config.ROOT_DIR + "data/fSin_samples_H05_T256_10Rev_10Diff_00Init.npy")
 idxs = np.arange(ddata.shape[0])
@@ -44,32 +44,57 @@ assert (prevPath_observations.shape[1] == ts_length)
 del path_observations
 
 
-# In[5]:
-
-
 def IID_NW_estimator(prevPath_observations, path_incs, bw, x, end_diff_time, start_diff_time):
     N, n = prevPath_observations.shape
-    kernel_weights_unnorm = gaussian_kernel(bw=bw, x=prevPath_observations[:, :, np.newaxis] - x)
+    k = prevPath_observations[:, :, np.newaxis] - x
+    kernel_weights_unnorm = gaussian_kernel(bw=bw, x=k)
     denominator = np.sum(kernel_weights_unnorm, axis=(1, 0)) / (N * n)
-    numerator = np.sum(kernel_weights_unnorm * path_incs[:, :, np.newaxis], axis=(1, 0)) / N * (
+    k = kernel_weights_unnorm * path_incs[:, :, np.newaxis]
+    numerator = np.sum(k, axis=(1, 0)) / N * (
             end_diff_time - start_diff_time)
     return numerator / denominator
 
 
-assert (prevPath_observations.shape[1] * Delta == (end_diff_time - start_diff_time))
+def vect_IID_NW_estimator(prevPath_observations, path_incs, bw, x, end_diff_time, start_diff_time):
+    N, N_minus1, n = prevPath_observations.shape
+    t0 = time.time()
+    k = prevPath_observations[:, :, :, np.newaxis] - x[:, np.newaxis, np.newaxis, :]
+    for i in range(0):
+        submat = prevPath_observations[i, :, :]
+        x1 = x[i, :]
+        k1 = submat[:, :, np.newaxis] - x1
+        k2 = k[i, :, :, :]
+        assert (np.allclose(k1, k2))
+    print(time.time() - t0)
+    t0 = time.time()
+    kernel_weights_unnorm = gaussian_kernel(bw=bw, x=k)
+    print(time.time() - t0)
+    denominator = np.sum(kernel_weights_unnorm, axis=(1, 2)) / (N * n)
+    k = kernel_weights_unnorm * path_incs[:, :, :, np.newaxis]
+    for i in range(0):
+        k1 = k[i, :, :, :]
+        k2 = kernel_weights_unnorm[i, :, :, :] * path_incs[i, :, :, np.newaxis]
+        assert (np.allclose(k1, k2))
+    numerator = np.sum(k, axis=(1, 2)) / N * (end_diff_time - start_diff_time)
+    return numerator / denominator
 
 
-# In[7]:
+def exclude_row(matrix):
+    M, N = matrix.shape
+    matrix = np.repeat(matrix[np.newaxis, :, :], M, axis=0)
+    # Create a mask for rows to exclude
+    mask = np.ones((M, M), dtype=bool)  # Start with all rows included
+    np.fill_diagonal(mask, False)
+    mask = np.concatenate([mask[:, :, np.newaxis]] * N, axis=-1)
+    # Apply mask to create (M, M-1, N)
+    result = matrix[mask].reshape(M, M - 1, N)
+    return result
+
 
 def compute_cv_for_bw_per_path(i, _bw, prevPath_observations, path_incs):
-    tracemalloc.start()
-    memory_check(str=f"Start of path {i} opt")
-    t0 = time.time()
     N = prevPath_observations.shape[0]
     mask = np.arange(N) != i
-    print(i)
     sys.stdout.flush()
-    memory_check(str=f"Start of estimator for path {i} opt")
     estimator = IID_NW_estimator(
         prevPath_observations=prevPath_observations[mask, :],
         path_incs=path_incs[mask, :],
@@ -78,37 +103,48 @@ def compute_cv_for_bw_per_path(i, _bw, prevPath_observations, path_incs):
         end_diff_time=end_diff_time,
         start_diff_time=start_diff_time
     )
-    memory_check(str=f"End of estimator for path {i} opt")
     residual = estimator ** 2 * Delta - 2 * estimator * path_incs[i, :]
     cv = np.sum(residual)
     if np.isnan(cv):
         return np.inf
-    print(time.time() - t0)
     return cv
 
 
 def compute_cv_for_bw(_bw, prevPath_observations, path_incs):
     N = prevPath_observations.shape[0]
-    print(f"Starting: {_bw}\n")
-    t0 = time.time()
     cvs = Parallel(n_jobs=30, backend="loky")(
         delayed(compute_cv_for_bw_per_path)(i, _bw, prevPath_observations, path_incs) for i in range(N))
-    print(time.time() - t0)
     return np.sum(cvs)
 
-memory_check(str=f"BW optimatisation done")
+
+assert (prevPath_observations.shape[1] * Delta == (end_diff_time - start_diff_time))
+
+
+def compute_cv_for_bw_vectorised(_bw, prevPath_observations, path_incs, bw_obs, bw_incs):
+    vec_estimator = vect_IID_NW_estimator(bw_obs, bw_incs, _bw, prevPath_observations, end_diff_time, start_diff_time)
+    residual = vec_estimator ** 2 * Delta - 2 * vec_estimator * path_incs
+    return {_bw: np.sum(residual)}
+
 
 bws = np.logspace(-2, 0, 20)
-mask = np.ones(prevPath_observations.shape[0], dtype=bool)
-CVs = np.zeros(len(bws))
-print(bws.shape)
-for h in tqdm(range(bws.shape[0])):
-    CVs[h] = compute_cv_for_bw(bws[h], prevPath_observations, path_incs)
+if num_paths > 1:
+    bw_obs = exclude_row(prevPath_observations)
+    bw_incs = exclude_row(path_incs)
+    list_cvs = Parallel(n_jobs=1, backend="loky")(
+        delayed(compute_cv_for_bw_vectorised)(_bw, prevPath_observations, path_incs, bw_obs, bw_incs) for _bw in bws)
+    CVs = {}
+    # Merge all dictionaries into the single dictionary
+    for d in list_cvs:
+        CVs.update(d)
+    print(CVs)
+else:
+    mask = np.ones(prevPath_observations.shape[0], dtype=bool)
+    CVs = np.zeros(len(bws))
+    for h in tqdm(range(bws.shape[0])):
+        CVs[h] = compute_cv_for_bw(bws[h], prevPath_observations, path_incs)
 
 # In[ ]:
-
-
-bw = bws[np.argmin(CVs)]
+bw = bws[np.argmin(CVs.values())]
 print(bw, CVs)
 
 # In[45]:
