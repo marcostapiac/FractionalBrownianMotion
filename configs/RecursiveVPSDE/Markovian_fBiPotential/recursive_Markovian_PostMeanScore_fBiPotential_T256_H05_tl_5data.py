e978542fa72da6cc608feb6a5fb92d5d2c4e15a4
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-02-08 01:20:04 +0000

    BiPotential code

--- configs/RecursiveVPSDE/Markovian_fBiPotential/recursive_Markovian_PostMeanScore_fBiPotential_T256_H05_tl_5data.py
+++ configs/RecursiveVPSDE/Markovian_fBiPotential/recursive_Markovian_PostMeanScore_fBiPotential_T256_H05_tl_5data.py
@@ -18,7 +18,7 @@ def get_config():
     config.diffusion = 1.
     config.initState = 0.
     config.ts_length = 256
-    config.data_path = project_config.ROOT_DIR + "data/fQuadSin_samples_H{}_T{}_{}a_{}b_{}c_{}Diff_{}Init".format(
+    config.data_path = project_config.ROOT_DIR + "data/fBiPotential_samples_H{}_T{}_{}a_{}b_{}c_{}Diff_{}Init".format(
         str(config.hurst), config.ts_length, config.quad_coeff, config.sin_coeff, config.sin_space_scale,
         config.diffusion, config.initState).replace(
         ".", "") + ".npy"
@@ -55,14 +55,14 @@ def get_config():
     config.dialation_length = 10
 
     # Model filepath
-    mlpFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_rec_markv_PM_MLP_{}_incs_{}_unitIntv_fQuadSin_VPSDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_BetaMax{:.4e}_BetaMin{:.4e}_TembDim{}_EncShapes{}_tl5".format(
+    mlpFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_rec_markv_PM_MLP_{}_incs_{}_unitIntv_fBiPotential_VPSDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_BetaMax{:.4e}_BetaMin{:.4e}_TembDim{}_EncShapes{}_tl5".format(
         not config.isfBm, config.isUnitInterval, config.hurst,
         config.ts_length,
         config.max_diff_steps, config.end_diff_time, config.train_eps, config.beta_max, config.beta_min,
         config.temb_dim,
         config.enc_shapes).replace(".", "")
 
-    tsmFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_rec_markv_PM_TSM_{}_incs_{}_unitIntv_fQuadSin_VPSDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_BetaMax{:.4e}_BetaMin{:.4e}_DiffEmbSize{}_ResLay{}_ResChan{}_DiffHiddenSize{}_{}Hybrid_{}Wghts_{}a_{}b_{}c_tl{}".format(
+    tsmFileName = project_config.ROOT_DIR + "src/generative_modelling/trained_models/trained_rec_markv_PM_TSM_{}_incs_{}_unitIntv_fBiPotential_VPSDE_model_H{:.3e}_T{}_Ndiff{}_Tdiff{:.3e}_trainEps{:.0e}_BetaMax{:.4e}_BetaMin{:.4e}_DiffEmbSize{}_ResLay{}_ResChan{}_DiffHiddenSize{}_{}Hybrid_{}Wghts_{}a_{}b_{}c_tl{}".format(
         not config.isfBm, config.isUnitInterval, config.hurst,
         config.ts_length,
         config.max_diff_steps, config.end_diff_time, config.train_eps, config.beta_max, config.beta_min,
