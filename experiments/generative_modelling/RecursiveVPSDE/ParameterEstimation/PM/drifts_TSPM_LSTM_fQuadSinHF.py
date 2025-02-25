071f47b18947a6e00a327202a5dd856b7cf75fd3
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-02-25 17:02:39 +0000

    Puhsing LSTM drift check

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/drifts_TSPM_LSTM_fQuadSinHF.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/drifts_TSPM_LSTM_fQuadSinHF.py
@@ -1,18 +1,20 @@
-
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from configs.RecursiveVPSDE.Markovian_fQuadSinHF.recursive_Markovian_fQuadSinHFWithPosition_T256_H05_tl_110data import get_config
+from configs.RecursiveVPSDE.Markovian_fQuadSinHF.recursive_Markovian_PostMeanScore_fQuadSinHF_T256_H05_tl_110data import get_config as get_config
+
 from configs import project_config
 from src.generative_modelling.models.ClassVPSDEDiffusion import VPSDEDiffusion
-from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditionalMarkovianTSScoreMatching import \
-    ConditionalMarkovianTSScoreMatching
+from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditionalMarkovianTSPostMeanScoreMatching import \
+    ConditionalMarkovianTSPostMeanScoreMatching
 
 # In[22]:
 
 
 config = get_config()
+
 print("Beta Min : ", config.beta_min)
 if config.has_cuda:
     device = int(os.environ["LOCAL_RANK"])
@@ -28,14 +30,17 @@ ts_step = 1 / config.ts_length
 
 Nepoch = 960  # config.max_epochs[0]
 # Fix the number of training epochs and training loss objective loss
-PM = ConditionalMarkovianTSScoreMatching(*config.model_parameters).to(device)
+PM = ConditionalMarkovianTSPostMeanScoreMatching(*config.model_parameters).to(device)
 PM.load_state_dict(torch.load(config.scoreNet_trained_path + "_NEp" + str(Nepoch)))
 print(config.scoreNet_trained_path)
+
+
 # In[23]:
 
 
 Xshape = config.ts_length
 num_taus = 500
+
 num_diff_times = config.max_diff_steps
 Ndiff_discretisation = config.max_diff_steps
 diffusion_times = torch.linspace(start=config.sample_eps, end=config.end_diff_time,
@@ -43,7 +48,7 @@ diffusion_times = torch.linspace(start=config.sample_eps, end=config.end_diff_ti
 mu_hats_mean = np.zeros((Xshape, num_taus))
 mu_hats_std = np.zeros((Xshape, num_taus))
 
-Xs = torch.linspace(-2.5, 2.5, steps=Xshape).unsqueeze(-1).unsqueeze(-1).permute(1, 0, 2).to(device)
+Xs = torch.linspace(-1.2, 1.2, steps=Xshape).unsqueeze(-1).unsqueeze(-1).permute(1, 0, 2).to(device)
 conditioner = torch.stack([Xs for _ in range(1)], dim=0).reshape(Xshape * 1, 1, -1)
 B, T = Xshape, 1
 final_vec_mu_hats = np.zeros((Xshape, num_diff_times, num_taus))  # Xvalues, DiffTimes, Ztaus
@@ -51,7 +56,8 @@ PM.eval()
 vec_Z_taus = diffusion.prior_sampling(shape=(Xshape*num_taus, 1, 1)).to(device)
 difftime_idx = num_diff_times - 1
 ts = []
-while difftime_idx >= 0:
+es = 100
+while difftime_idx >= num_diff_times - es:
     d = diffusion_times[Ndiff_discretisation - (num_diff_times - 1 - difftime_idx) - 1].to(device)
     # I (will) have a RV for each x (there are B of them) and hence need a diffusion time for each one
     diff_times = torch.stack([d for _ in range(B)]).reshape(B * T).to(device)
@@ -61,7 +67,8 @@ while difftime_idx >= 0:
     vec_conditioner = torch.stack([conditioner for _ in range(num_taus)], dim=0).reshape(num_taus*Xshape, 1, 1)
 
     with torch.no_grad():
-        vec_predicted_score = PM.forward(inputs=vec_Z_taus, times=vec_diff_times, conditioner=vec_conditioner)
+        vec_predicted_score = PM.forward(inputs=vec_Z_taus, times=vec_diff_times, conditioner=vec_conditioner,
+                                         eff_times=vec_eff_times)
     vec_scores, vec_drift, vec_diffParam = diffusion.get_conditional_reverse_diffusion(x=vec_Z_taus,
                                                                            predicted_score=vec_predicted_score,
                                                                            diff_index=torch.Tensor(
@@ -80,11 +87,15 @@ while difftime_idx >= 0:
     vec_Z_taus = vec_drift + vec_diffParam * vec_z
     difftime_idx -= 1
 
-type = "Standard"
-assert ("PM" not in config.scoreNet_trained_path)
+
+type = "PM"
+assert (type in config.scoreNet_trained_path)
 print(type)
 save_path = (
-            project_config.ROOT_DIR + f"experiments/results/TS_mkv_fQuadSinHF_DriftEvalExp_{Nepoch}Nep_{config.loss_factor}LFactor_{config.t0}t0_{config.deltaT:.3e}dT_{config.quad_coeff}a_{config.sin_coeff}b_{config.sin_space_scale}c_{config.max_diff_steps}DiffSteps").replace(
-        ".", "")
+        project_config.ROOT_DIR + f"experiments/results/TSPM_mkv_fQuadSinHF_DriftEvalExp_{Nepoch}Nep_{config.loss_factor}LFactor_{config.t0}t0_{config.deltaT:.3e}dT_{config.quad_coeff}a_{config.sin_coeff}b_{config.sin_space_scale}c_{config.max_diff_steps}DiffSteps").replace(
+    ".", "")
 print(save_path)
-np.save(save_path + "_muhats.npy", final_vec_mu_hats)
+if es == 1:
+    np.save(save_path + "_muhats.npy", final_vec_mu_hats[:, [-1], :])
+else:
+    np.save(save_path + "_muhats.npy", final_vec_mu_hats[:, -es:, :])
