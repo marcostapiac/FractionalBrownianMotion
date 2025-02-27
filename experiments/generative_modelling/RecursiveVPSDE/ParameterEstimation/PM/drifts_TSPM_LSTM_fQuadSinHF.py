ec52542c3795bf3431bb43375ac0e2cbdd32d186
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-02-27 15:43:37 +0000

    Checking if deltaT is problem with super HF

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/drifts_TSPM_LSTM_fQuadSinHF.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/drifts_TSPM_LSTM_fQuadSinHF.py
@@ -77,7 +77,7 @@ max_diff_steps = config.max_diff_steps
 sample_eps = config.sample_eps
 ts_step = config.deltaT
 
-Nepoch = 1920
+Nepoch = 300
 # Fix the number of training epochs and training loss objective loss
 PM = ConditionalLSTMTSPostMeanScoreMatching(*config.model_parameters).to(device)
 PM.load_state_dict(torch.load(config.scoreNet_trained_path + "_NEp" + str(Nepoch)))
@@ -91,7 +91,7 @@ Ndiff_discretisation = config.max_diff_steps
 diffusion_times = torch.linspace(start=config.sample_eps, end=config.end_diff_time,
                                  steps=Ndiff_discretisation).to(device)
 
-Xs = torch.linspace(-.4, .4, steps=Xshape)
+Xs = torch.linspace(-1.2, 1.2, steps=Xshape)
 features = find_LSTM_feature_vectors(Xs=Xs, PM=PM, device=device, config=config)
 num_feats_per_x = {x.item(): features[x.item()].shape[0] for x in Xs}
 list_num_feats_per_x = list(num_feats_per_x.values())
