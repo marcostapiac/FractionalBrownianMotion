d7e567bc05207146f92518ee12862b6d1f31921a
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-06 13:53:30 +0000

    Pushing ready for LSTM BiPot

--- experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/drifts_TSPM_LSTM_fBiPot.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParameterEstimation/PM/drifts_TSPM_LSTM_fBiPot.py
@@ -74,7 +74,7 @@ max_diff_steps = config.max_diff_steps
 sample_eps = config.sample_eps
 ts_step = config.deltaT
 
-Nepoch = 300
+Nepoch = 960
 # Fix the number of training epochs and training loss objective loss
 PM = ConditionalLSTMTSPostMeanScoreMatching(*config.model_parameters).to(device)
 PM.load_state_dict(torch.load(config.scoreNet_trained_path + "_NEp" + str(Nepoch)))
