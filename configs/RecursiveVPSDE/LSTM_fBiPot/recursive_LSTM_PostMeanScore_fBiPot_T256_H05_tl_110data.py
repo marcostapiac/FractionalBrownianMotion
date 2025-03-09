96475fbc5f3e37b8fb28bfe04b5491cabc026b6d
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-09 15:55:16 +0000

    Checking scaling loss for bipot for improved training

--- configs/RecursiveVPSDE/LSTM_fBiPot/recursive_LSTM_PostMeanScore_fBiPot_T256_H05_tl_110data.py
+++ configs/RecursiveVPSDE/LSTM_fBiPot/recursive_LSTM_PostMeanScore_fBiPot_T256_H05_tl_110data.py
@@ -30,7 +30,7 @@ def get_config():
     config.train_eps = 1./config.max_diff_steps  # 1000 * max(int(np.log2(config.ts_length) - 1), 1)
     config.end_diff_time = 1.
     config.save_freq = 50
-    config.lr = 1e-2
+    config.lr = 1e-3
     config.max_epochs = [60, 100, 150, 300, 960, 1440, 1920, 2920, 6920, 7190, 8190, 12920]
     config.batch_size = 256
     config.isfBm = True
