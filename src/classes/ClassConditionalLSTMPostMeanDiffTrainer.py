a00947bda9360f0b07b7f7de04dc34c6f67063ed
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-12 07:24:34 +0000

    Removing scheduler

--- src/classes/ClassConditionalLSTMPostMeanDiffTrainer.py
+++ src/classes/ClassConditionalLSTMPostMeanDiffTrainer.py
@@ -74,7 +74,7 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
             :return: Batch Loss
         """
         loss.backward()  # single gpu functionality
-        self.opt.optimizer.step()
+        #self.opt.optimizer.step()
         self.opt.step()
         # Detach returns the loss as a Tensor that does not require gradients, so you can manipulate it
         # independently of the original value, which does require gradients
@@ -103,7 +103,8 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
             :param eff_times: Effective diffusion times
             :return: Batch Loss
         """
-        self.opt.optimizer.zero_grad()
+        #self.opt.optimizer.zero_grad()
+        self.opt.zero_grad()
         B, T, D = xts.shape
         # Reshaping concatenates vectors in dim=1
         xts = xts.reshape(B * T, 1, -1)
