e9c1705b3dd9be6dbc484f1924f491ff62aa2d9e
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-26 16:03:15 +0000

    Pushing multi dim setups

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/LSTM/drifts_TSPM_LSTM_fMullerBrown_TrackingErrors.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/LSTM/drifts_TSPM_LSTM_fMullerBrown_TrackingErrors.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
         PM.load_state_dict(torch.load(config.scoreNet_trained_path + "_NEp" + str(Nepoch)))
         PM = PM.to(device)
 
-        num_paths = 10
+        num_paths = 100
         num_time_steps = 100
         deltaT = config.deltaT
         initial_state = np.repeat(np.array(config.initState)[np.newaxis, np.newaxis, :], num_paths, axis=0)
