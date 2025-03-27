ebb95b4a5c65a77727ecbf9c7f668d63b0e9e811
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-27 15:40:14 +0000

    Starting to consider confidence bands for accurate comparison

---
+++ utils/drift_evaluation_functions.py
@@ -0,0 +1,126 @@
+import torch
+import numpy as np
+from torch.nn.utils.rnn import pad_sequence
+
+
+def multivar_score_based_LSTM_drift_OOS(score_model, time_idx, h, c, num_diff_times, diffusion, num_paths, prev,
+                                        ts_step, config,
+                                        device):
+    num_taus = 100
+    Ndiff_discretisation = config.max_diff_steps
+    assert (prev.shape == (num_paths, config.ndims))
+    score_model.eval()
+    with torch.no_grad():
+        if time_idx == 0:
+            features, (h, c) = score_model.rnn(torch.tensor(prev[:, np.newaxis, :], dtype=torch.float32).to(device),
+                                               None)
+        else:
+            features, (h, c) = score_model.rnn(torch.tensor(prev[:, np.newaxis, :], dtype=torch.float32).to(device),
+                                               (h, c))
+    assert (prev[0, :].shape[0] == config.ts_dims)
+    if prev[0, :].shape[0] > 1:
+        features = {tuple(prev[i, :].squeeze().tolist()): features[i] for i in range(prev.shape[0])}
+        num_feats_per_x = {tuple(x.squeeze().tolist()): features[tuple(x.squeeze().tolist())].shape[0] for x in prev}
+    else:
+        features = {prev[i, :].item(): features[i] for i in range(prev.shape[0])}
+        num_feats_per_x = {x.item(): features[x.item()].shape[0] for x in prev}
+    # list_num_feats_per_x = list(num_feats_per_x.values())
+    tot_num_feats = np.sum(list(num_feats_per_x.values()))
+    features_tensor = torch.concat(list(features.values()), dim=0).to(device)  # [num_features_per_x, 1, 20]
+    assert (features_tensor.shape[0] == tot_num_feats)
+    vec_Z_taus = diffusion.prior_sampling(shape=(tot_num_feats * num_taus, 1, config.ts_dims)).to(device)
+
+    diffusion_times = torch.linspace(config.sample_eps, 1., config.max_diff_steps)
+    difftime_idx = Ndiff_discretisation - 1
+    while difftime_idx >= Ndiff_discretisation - num_diff_times:
+        d = diffusion_times[difftime_idx].to(device)
+        diff_times = torch.stack([d for _ in range(tot_num_feats)]).reshape(tot_num_feats * 1).to(device)
+        eff_times = diffusion.get_eff_times(diff_times=diff_times).unsqueeze(-1).unsqueeze(-1).to(device)
+        vec_diff_times = torch.stack([diff_times for _ in range(num_taus)], dim=0).reshape(num_taus * tot_num_feats)
+        vec_eff_times = torch.stack([eff_times for _ in range(num_taus)], dim=0).reshape(num_taus * tot_num_feats, 1, 1)
+        vec_conditioner = torch.stack([features_tensor for _ in range(num_taus)], dim=0).reshape(
+            num_taus * tot_num_feats,
+            1, -1)
+        score_model.eval()
+        with torch.no_grad():
+            vec_predicted_score = score_model.forward(times=vec_diff_times, eff_times=vec_eff_times,
+                                                      conditioner=vec_conditioner, inputs=vec_Z_taus)
+        vec_scores, vec_drift, vec_diffParam = diffusion.get_conditional_reverse_diffusion(x=vec_Z_taus,
+                                                                                           predicted_score=vec_predicted_score,
+                                                                                           diff_index=torch.Tensor(
+                                                                                               [int((
+                                                                                                       num_diff_times - 1 - difftime_idx))]).to(
+                                                                                               device),
+                                                                                           max_diff_steps=Ndiff_discretisation)
+        # assert np.allclose((scores- predicted_score).detach(), 0)
+        beta_taus = torch.exp(-0.5 * eff_times[0, 0, 0]).to(device)
+        sigma_taus = torch.pow(1. - torch.pow(beta_taus, 2), 0.5).to(device)
+        final_mu_hats = (vec_Z_taus / (ts_step * beta_taus)) + ((
+                                                                        (torch.pow(sigma_taus, 2) + (
+                                                                                torch.pow(beta_taus, 2) * ts_step)) / (
+                                                                                ts_step * beta_taus)) * vec_scores)
+
+        assert (final_mu_hats.shape == (num_taus * tot_num_feats, 1, config.ts_dims))
+        A = final_mu_hats.reshape((num_taus, tot_num_feats, config.ts_dims))
+        split_tensors = torch.split(A, list(num_feats_per_x.values()), dim=1)
+        # Compute means along the column dimension
+        means = torch.stack([t.mean(dim=1) for t in split_tensors], dim=1)
+        assert (means.shape == (num_taus, num_paths, config.ts_dims))
+
+        # print(vec_Z_taus.shape, vec_scores.shape)
+        vec_z = torch.randn_like(vec_drift).to(device)
+        vec_Z_taus = vec_drift + vec_diffParam * vec_z
+        difftime_idx -= 1
+    return means.mean(dim=0).reshape(num_paths, 1, config.ts_dims).cpu().numpy(), h, c
+
+
+
+def find_LSTM_feature_vectors_multiDTS(Xs, PM, config, device):
+    sim_data = np.load(config.data_path, allow_pickle=True)
+    sim_data_tensor = torch.tensor(sim_data, dtype=torch.float).to(device)
+
+    def process_single_threshold(x, dX_global):
+        diff = sim_data_tensor - x.reshape(1, -1)  # shape: (M, N, D)
+        diff = diff.norm(dim=-1)  # Result: (M, N)
+        mask = diff <= torch.min(diff)
+
+        """mask = diff <=np.arccos(dX_global)
+        thresh = dX_global
+        while torch.sum(mask) == 0:
+            thresh = np.cos(np.arccos(thresh)*2)
+            mask = diff <= np.arccos(thresh)
+        assert (torch.sum(mask) > 0)"""
+
+        # Get indices where mask is True (each index is [i, j])
+        indices = mask.nonzero(as_tuple=False)
+        sequences = []
+        js = []
+        for k in range(min(100, indices.shape[0])):
+            idx = indices[k, :]
+            i, j = idx.tolist()
+            # Extract the sequence: row i, columns 0 to j (inclusive)
+            seq = sim_data_tensor[i, :j + 1, :]
+            sequences.append(seq)
+            js.append(len(seq))
+        outputs = []
+        PM.eval()
+        if sequences:
+            # Pad sequences to create a batch.
+            # pad_sequence returns tensor of shape (batch_size, max_seq_len)
+            padded_batch = pad_sequence(sequences, batch_first=True, padding_value=torch.nan).to(device)
+            # Add feature dimension: now shape becomes (batch_size, max_seq_len, 1)
+            # padded_batch = padded_batch.unsqueeze(-1).to(device)
+            with torch.no_grad():
+                batch_output, _ = PM.rnn(padded_batch, None)
+            outputs = batch_output[torch.arange(batch_output.shape[0]), torch.tensor(js, dtype=torch.long) - 1,
+                      :].unsqueeze(1).cpu()
+        return x, outputs
+
+    features_Xs = {}
+    dX_global = np.cos(1. / 5000)
+    for i in range(Xs.shape[0]):
+        x = Xs[i, :].reshape(-1, 1)
+        x_val, out = process_single_threshold(torch.tensor(x).to(device, dtype=torch.float), dX_global)
+        assert (len(out) > 0)
+        features_Xs[tuple(x.squeeze().tolist())] = out
+    return features_Xs
