c25c87044e69e8b56a5bb7293e6b240d4882fc39
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-27 14:15:00 +0000

    Starting to consider confidence bands for accurate comparison

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/LSTM/drifts_TSPM_LSTM_fBiPot.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/LSTM/drifts_TSPM_LSTM_fBiPot.py
@@ -81,7 +81,7 @@ def LSTM_1D_drifts(config, PM):
     ts_step = config.deltaT
     print(config.scoreNet_trained_path)
     Xshape = config.ts_length
-    num_taus = 500
+    num_taus = 100
 
     num_diff_times = config.max_diff_steps
     Ndiff_discretisation = config.max_diff_steps
