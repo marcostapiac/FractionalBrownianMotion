c290adcdc718b5a6e60e8d965c8a1e12bfb04be2
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-28 10:39:26 +0000

    Pushing nadaraya tracking with confidence intervals

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/benchmark_IIDNadaraya_BiPot_Tracking_Parallelised.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/benchmark_IIDNadaraya_BiPot_Tracking_Parallelised.py
@@ -8,59 +8,15 @@ from utils.drift_evaluation_functions import IID_NW_multivar_estimator
 import multiprocessing as mp
 from multiprocessing import shared_memory
 
+
 def true_drift(prev, num_paths, config):
     assert (prev.shape == (num_paths, config.ndims))
     drift_X = -(4. * config.quartic_coeff * np.power(prev, 3) + 2. * config.quad_coeff * prev + config.const)
     return drift_X[:, np.newaxis, :]
 
 
-
-config = get_config()
-num_paths = 109
-num_time_steps = config.ts_length
-isUnitInterval = True
-diff = config.diffusion
-initial_state = config.initState
-rvs = None
-H = config.hurst
-deltaT = config.deltaT
-t0 = config.t0
-t1 = deltaT * num_time_steps
-fBiPot = FractionalBiPotential(const=config.const, quartic_coeff=config.quartic_coeff, quad_coeff=config.quad_coeff,
-                               diff=diff, X0=initial_state)
-is_path_observations = np.array(
-    [fBiPot.euler_simulation(H=H, N=num_time_steps, deltaT=deltaT, isUnitInterval=isUnitInterval, X0=initial_state,
-                             Ms=None, gaussRvs=rvs,
-                             t0=t0, t1=t1) for _ in (range(num_paths))]).reshape(
-    (num_paths, num_time_steps + 1))
-
-is_idxs = np.arange(is_path_observations.shape[0])
-path_observations = is_path_observations[np.random.choice(is_idxs, size=num_paths, replace=False), :]
-# We note that we DO NOT evaluate the drift at time t_{0}=0
-# We therefore remove the first element of path_observations since it includes X_{t_{0}} = X_{0}
-# We also remove the last element since we never evaluate the drift at that point
-t0 = deltaT
-prevPath_observations = path_observations[:, 1:-1]
-# We compute the path incs with respect to the prevPath_observations (since X_{t_{0}} != X_{0})
-path_incs = np.diff(path_observations, axis=1)[:, 1:]
-assert (prevPath_observations.shape == path_incs.shape)
-assert (path_incs.shape[1] == config.ts_length - 1)
-assert (path_observations.shape[1] == prevPath_observations.shape[1] + 2)
-assert (prevPath_observations.shape[1] * deltaT == (t1 - t0))
-bws = np.logspace(-4, -0.05, 6)
-
-prevPath_shm = shared_memory.SharedMemory(create=True, size=prevPath_observations.nbytes)
-path_incs_shm = shared_memory.SharedMemory(create=True, size=path_incs.nbytes)
-
-# Create numpy arrays from the shared memory buffers
-prevPath_shm_array = np.ndarray(prevPath_observations.shape, dtype=np.float64, buffer=prevPath_shm.buf)
-path_incs_shm_array = np.ndarray(path_incs.shape, dtype=np.float64, buffer=path_incs_shm.buf)
-
-# Copy the data into the shared memory arrays
-np.copyto(prevPath_shm_array, prevPath_observations)
-np.copyto(path_incs_shm_array, path_incs)
-
-def process_bandwidth(bw_idx, bw, shape, config, rmse_quantile_nums, num_paths, num_time_steps, num_state_paths, deltaT, prevPath_name, path_incs_name):
+def process_bandwidth(bw_idx, bw, shape, config, rmse_quantile_nums, num_time_steps, num_state_paths, deltaT,
+                      prevPath_name, path_incs_name):
     bw = np.array([bw])
     inv_H = np.diag(np.power(bw, -2))
     norm_const = 1 / np.sqrt((2. * np.pi) ** config.ndims * (1. / np.linalg.det(inv_H)))
@@ -101,31 +57,81 @@ def process_bandwidth(bw_idx, bw, shape, config, rmse_quantile_nums, num_paths, 
         all_true_states[quant_idx, :, :, :] = true_states
         # all_global_states[quant_idx, :, :, :] = global_states
         all_local_states[quant_idx, :, :, :] = local_states
-    return all_true_states, all_global_states, all_local_states
-
-num_time_steps = 100
-num_state_paths = 100
-rmse_quantile_nums = 20
-# Euler-Maruyama Scheme for Tracking Errors
-shape = prevPath_observations.shape
+    return {bw_idx: (all_true_states, all_global_states, all_local_states)}
 
 
-with mp.Pool(processes=6) as pool:
-    # Prepare the arguments for each task
-    tasks = [(idx, bw, shape, config, rmse_quantile_nums, num_paths, num_time_steps, num_state_paths, deltaT, prevPath_shm.name, path_incs_shm.name) for idx, bw in enumerate(bws)]
+if __name__ == '__main__':
+    config = get_config()
+    num_paths = 10952
+    num_time_steps = config.ts_length
+    isUnitInterval = True
+    diff = config.diffusion
+    initial_state = config.initState
+    rvs = None
+    H = config.hurst
+    deltaT = config.deltaT
+    t0 = config.t0
+    t1 = deltaT * num_time_steps
+    fBiPot = FractionalBiPotential(const=config.const, quartic_coeff=config.quartic_coeff, quad_coeff=config.quad_coeff,
+                                   diff=diff, X0=initial_state)
+    is_path_observations = np.array(
+        [fBiPot.euler_simulation(H=H, N=num_time_steps, deltaT=deltaT, isUnitInterval=isUnitInterval, X0=initial_state,
+                                 Ms=None, gaussRvs=rvs,
+                                 t0=t0, t1=t1) for _ in (range(num_paths))]).reshape(
+        (num_paths, num_time_steps + 1))
 
-    # Run the tasks in parallel
-    results = pool.starmap(process_bandwidth, tasks)
+    is_idxs = np.arange(is_path_observations.shape[0])
+    path_observations = is_path_observations[np.random.choice(is_idxs, size=num_paths, replace=False), :]
+    # We note that we DO NOT evaluate the drift at time t_{0}=0
+    # We therefore remove the first element of path_observations since it includes X_{t_{0}} = X_{0}
+    # We also remove the last element since we never evaluate the drift at that point
+    t0 = deltaT
+    prevPath_observations = path_observations[:, 1:-1]
+    # We compute the path incs with respect to the prevPath_observations (since X_{t_{0}} != X_{0})
+    path_incs = np.diff(path_observations, axis=1)[:, 1:]
+    assert (prevPath_observations.shape == path_incs.shape)
+    assert (path_incs.shape[1] == config.ts_length - 1)
+    assert (path_observations.shape[1] == prevPath_observations.shape[1] + 2)
+    assert (prevPath_observations.shape[1] * deltaT == (t1 - t0))
+    bws = np.logspace(-4, -0.05, 6)
 
-# Cleanup shared memory
-prevPath_shm.close()
-path_incs_shm.close()
-prevPath_shm.unlink()
-path_incs_shm.unlink()
-"""save_path = (
-            project_config.ROOT_DIR + f"experiments/results/IIDNadaraya_fBiPot_DriftTrack_{round(bw[0], 6)}bw_{num_paths}NPaths_{config.t0}t0_{config.deltaT:.3e}dT_{config.quartic_coeff}a_{config.quad_coeff}b_{config.const}c").replace(
-        ".", "")
-    np.save(save_path + "_true_states.npy", all_true_states)
-    np.save(save_path + "_global_states.npy", all_global_states)
-    np.save(save_path + "_local_states.npy", all_local_states)
-"""
+    prevPath_shm = shared_memory.SharedMemory(create=True, size=prevPath_observations.nbytes)
+    path_incs_shm = shared_memory.SharedMemory(create=True, size=path_incs.nbytes)
+
+    # Create numpy arrays from the shared memory buffers
+    prevPath_shm_array = np.ndarray(prevPath_observations.shape, dtype=np.float64, buffer=prevPath_shm.buf)
+    path_incs_shm_array = np.ndarray(path_incs.shape, dtype=np.float64, buffer=path_incs_shm.buf)
+
+    # Copy the data into the shared memory arrays
+    np.copyto(prevPath_shm_array, prevPath_observations)
+    np.copyto(path_incs_shm_array, path_incs)
+
+    num_time_steps = 100
+    num_state_paths = 100
+    rmse_quantile_nums = 20
+    # Euler-Maruyama Scheme for Tracking Errors
+    shape = prevPath_observations.shape
+
+    with mp.Pool(processes=bws.shape[0]) as pool:
+        # Prepare the arguments for each task
+        tasks = [(idx, bw, shape, config, rmse_quantile_nums, num_time_steps, num_state_paths, deltaT,
+                  prevPath_shm.name, path_incs_shm.name) for idx, bw in enumerate(bws)]
+
+        # Run the tasks in parallel
+        results = pool.starmap(process_bandwidth, tasks)
+    results = {k: v for d in results for k, v in d.items()}
+    # Cleanup shared memory
+    prevPath_shm.close()
+    path_incs_shm.close()
+    prevPath_shm.unlink()
+    path_incs_shm.unlink()
+    for k in range(bws.shape[0]):
+        all_true_states, all_global_states, all_local_states = results[k]
+        save_path = (
+                    project_config.ROOT_DIR + f"experiments/results/IIDNadaraya_fBiPot_DriftTrack_{round(bws[0], 6)}bw_{num_paths}NPaths_{config.t0}t0_{config.deltaT:.3e}dT_{config.quartic_coeff}a_{config.quad_coeff}b_{config.const}c").replace(
+                ".", "")
+        print(all_true_states.shape, all_global_states.shape, all_local_states.shape)
+        np.save(save_path + "_true_states.npy", all_true_states)
+        np.save(save_path + "_global_states.npy", all_global_states)
+        np.save(save_path + "_local_states.npy", all_local_states)
+
