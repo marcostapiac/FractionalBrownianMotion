261ce92b9bc91d2f5218544ccfbec405437a3708
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-03-31 17:06:49 +0100

    Redoing WRMSE

--- src/classes/ClassConditionalLSTMPostMeanDiffTrainer.py
+++ src/classes/ClassConditionalLSTMPostMeanDiffTrainer.py
@@ -18,6 +18,7 @@ from src.generative_modelling.models.ClassVESDEDiffusion import VESDEDiffusion
 from src.generative_modelling.models.ClassVPSDEDiffusion import VPSDEDiffusion
 from src.generative_modelling.models.TimeDependentScoreNetworks.ClassConditionalLSTMTSPostMeanScoreMatching import \
     ConditionalLSTMTSPostMeanScoreMatching
+from utils.drift_evaluation_functions import multivar_score_based_LSTM_drift_OOS
 
 
 # Link for DDP vs DataParallelism: https://www.run.ai/guides/multi-gpu/pytorch-multi-gpu-4-techniques-explained
@@ -31,7 +32,8 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
         ConditionalLSTMTSPostMeanScoreMatching], train_data_loader: torch.utils.data.dataloader.DataLoader,
                  train_eps: float, end_diff_time: float, max_diff_steps: int, optimiser: torch.optim.Optimizer,
                  snapshot_path: str, device: Union[torch.device, int], checkpoint_freq: int, to_weight: bool,
-                 hybrid_training: bool,init_state: torch.Tensor, loss_factor: float, deltaT:float,loss_fn: callable = torch.nn.MSELoss,
+                 hybrid_training: bool, init_state: torch.Tensor, loss_factor: float, deltaT: float,
+                 loss_fn: callable = torch.nn.MSELoss,
                  loss_aggregator: torchmetrics.aggregation = MeanMetric):
         super().__init__()
         self.device_id = device
@@ -77,7 +79,7 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
             :return: Batch Loss
         """
         loss.backward()  # single gpu functionality
-        #self.opt.optimizer.step()
+        # self.opt.optimizer.step()
         self.opt.step()
         # Detach returns the loss as a Tensor that does not require gradients, so you can manipulate it
         # independently of the original value, which does require gradients
@@ -106,7 +108,7 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
             :param eff_times: Effective diffusion times
             :return: Batch Loss
         """
-        #self.opt.optimizer.zero_grad()
+        # self.opt.optimizer.zero_grad()
         self.opt.zero_grad()
         B, T, D = xts.shape
         # Reshaping concatenates vectors in dim=1
@@ -114,7 +116,7 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
         features = features.reshape(B * T, 1, -1)
         target_scores = target_scores.reshape(B * T, 1, -1)
         diff_times = diff_times.reshape(B * T)
-        eff_times = torch.cat([eff_times]*D, dim=2).reshape(target_scores.shape)
+        eff_times = torch.cat([eff_times] * D, dim=2).reshape(target_scores.shape)
         outputs = self.score_network.forward(inputs=xts, conditioner=features, times=diff_times, eff_times=eff_times)
         # For times larger than tau0, use inverse_weighting
         sigma_tau = 1. - torch.exp(-eff_times)
@@ -123,10 +125,10 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
             weights = torch.ones_like(eff_times)
         elif self.loss_factor == 0:  # PM
             weights = (sigma_tau / beta_tau)
-        elif self.loss_factor == 1: # PMScaled (meaning not scaled)
+        elif self.loss_factor == 1:  # PMScaled (meaning not scaled)
             weights = self.diffusion.get_loss_weighting(eff_times=eff_times)
         elif self.loss_factor == 2:  # PM with deltaT scaling
-            weights = (sigma_tau / (beta_tau*torch.sqrt(self.deltaT)))
+            weights = (sigma_tau / (beta_tau * torch.sqrt(self.deltaT)))
         # Outputs should be (NumBatches, TimeSeriesLength, 1)
         return self._batch_loss_compute(outputs=outputs * weights, targets=target_scores * weights)
 
@@ -289,6 +291,85 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
         self.score_network.module.train()
         self.score_network.module.to(self.device_id)
 
+    def _tracking_errors(self, epoch, config):
+        def true_drift(prev, num_paths, config):
+            assert (prev.shape == (num_paths, config.ndims))
+            drift_X = -(4. * config.quartic_coeff * np.power(prev, 3) + 2. * config.quad_coeff * prev + config.const)
+            return drift_X[:, np.newaxis, :]
+
+        diffusion = VPSDEDiffusion(beta_max=config.beta_max, beta_min=config.beta_min)
+        num_diff_times = 1
+        rmse_quantile_nums = 20
+        num_paths = 100
+        num_time_steps = 100
+        all_true_states = np.zeros(shape=(rmse_quantile_nums, num_paths, 1 + num_time_steps, config.ndims))
+        all_global_states = np.zeros(shape=(rmse_quantile_nums, num_paths, 1 + num_time_steps, config.ndims))
+        all_local_states = np.zeros(shape=(rmse_quantile_nums, num_paths, 1 + num_time_steps, config.ndims))
+        for quant_idx in range(rmse_quantile_nums):
+            self.score_network.module.eval()
+            num_paths = 100
+            num_time_steps = 100
+            deltaT = config.deltaT
+            initial_state = np.repeat(np.atleast_2d(config.initState)[np.newaxis, :], num_paths, axis=0)
+            assert (initial_state.shape == (num_paths, 1, config.ndims))
+
+            true_states = np.zeros(shape=(num_paths, 1 + num_time_steps, config.ndims))
+            global_states = np.zeros(shape=(num_paths, 1 + num_time_steps, config.ndims))
+            local_states = np.zeros(shape=(num_paths, 1 + num_time_steps, config.ndims))
+
+            # Initialise the "true paths"
+            true_states[:, [0], :] = initial_state + 0.00001 * np.random.randn(*initial_state.shape)
+            # Initialise the "global score-based drift paths"
+            global_states[:, [0], :] = true_states[:, [0], :]
+            local_states[:, [0], :] = true_states[:, [0],
+                                      :]  # np.repeat(initial_state[np.newaxis, :], num_diff_times, axis=0)
+
+            # Euler-Maruyama Scheme for Tracking Errors
+            global_h, global_c = None, None
+            local_h, local_c = None, None
+            for i in range(1, num_time_steps + 1):
+                eps = np.random.randn(num_paths, 1, config.ndims) * np.sqrt(deltaT)
+                assert (eps.shape == (num_paths, 1, config.ndims))
+                true_mean = self.__true_drift(true_states[:, i - 1, :], num_paths=num_paths, config=config)
+
+                true_states[:, [i], :] = true_states[:, [i - 1], :] \
+                                         + true_mean * deltaT \
+                                         + eps
+                global_mean, global_h, global_c = multivar_score_based_LSTM_drift_OOS(
+                    score_model=self.score_network.module, time_idx=i - 1,
+                    h=global_h, c=global_c,
+                    num_diff_times=num_diff_times,
+                    diffusion=diffusion,
+                    num_paths=num_paths,
+                    ts_step=deltaT, config=config,
+                    device=self.device_id,
+                    prev=global_states[:, i - 1, :])
+
+                global_states[:, [i], :] = global_states[:, [i - 1], :] + global_mean * deltaT + eps
+                local_mean, local_h, local_c = multivar_score_based_LSTM_drift_OOS(
+                    score_model=self.score_network.module, time_idx=i - 1,
+                    h=local_h, c=local_c,
+                    num_diff_times=num_diff_times,
+                    diffusion=diffusion,
+                    num_paths=num_paths, ts_step=deltaT,
+                    config=config,
+                    device=self.device_id,
+                    prev=true_states[:, i - 1, :])
+
+                local_states[:, [i], :] = true_states[:, [i - 1], :] + local_mean * deltaT + eps
+            all_true_states[quant_idx, :, :, :] = true_states
+            all_global_states[quant_idx, :, :, :] = global_states
+            all_local_states[quant_idx, :, :, :] = local_states
+        save_path = (
+                project_config.ROOT_DIR + f"experiments/results/TSPM_LSTM_fBiPot_OOSDriftTrack_{epoch}Nep_{config.t0}t0_{config.deltaT:.3e}dT_{config.quartic_coeff}a_{config.quad_coeff}b_{config.const}c_{config.residual_layers}ResLay_{config.loss_factor}LFac").replace(
+            ".", "")
+        print(f"Save path:{save_path}\n")
+        np.save(save_path + "_global_true_states.npy", all_true_states)
+        np.save(save_path + "_global_states.npy", all_global_states)
+        np.save(save_path + "_local_states.npy", all_local_states)
+        self.score_network.module.train()
+        self.score_network.module.to(self.device_id)
+
     def train(self, max_epochs: list, model_filename: str, config) -> None:
         """
         Run training for model
@@ -332,4 +413,5 @@ class ConditionalLSTMPostMeanDiffTrainer(nn.Module):
                     self._save_loss(losses=all_losses_per_epoch, filepath=model_filename)
                     self._save_snapshot(epoch=epoch)
                     self._domain_rmse(config=config, epoch=epoch + 1)
+                    self._tracking_errors(epoch=epoch+1, config=config)
             if type(self.device_id) == int: dist.barrier()
