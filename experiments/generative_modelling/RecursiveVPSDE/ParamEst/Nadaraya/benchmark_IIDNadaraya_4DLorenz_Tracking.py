d4d80ce41147c6d7ec43566559997bd2e6f26037
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-04-01 12:15:31 +0100

    More combined training for quadsinhf2

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/benchmark_IIDNadaraya_4DLorenz_Tracking.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/benchmark_IIDNadaraya_4DLorenz_Tracking.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     rmse_quantile_nums = 20
     # Euler-Maruyama Scheme for Tracking Errors
     shape = prevPath_observations.shape
-    for bw_idx in tqdm(30,range(bws.shape[0])):
+    for bw_idx in tqdm(range(bws.shape[0])):
         bw = bws[bw_idx,:]
         inv_H = np.diag(np.power(bw, -2))
         norm_const = 1 / np.sqrt((2. * np.pi) ** config.ndims * (1. / np.linalg.det(inv_H)))
