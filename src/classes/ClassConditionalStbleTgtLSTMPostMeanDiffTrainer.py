6ae0f2a4430c2f531906fcab140934d09b57878f
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-04-03 10:55:40 +0100

    Stable Target quadsinhf

--- src/classes/ClassConditionalStbleTgtLSTMPostMeanDiffTrainer.py
+++ src/classes/ClassConditionalStbleTgtLSTMPostMeanDiffTrainer.py
@@ -434,9 +434,14 @@ class ConditionalStbleTgtLSTMPostMeanDiffTrainer(nn.Module):
                         project_config.ROOT_DIR + f"experiments/results/TSPM_LSTM_fBiPot_DriftEvalExp_{epoch}Nep_{config.t0}t0_{config.deltaT:.3e}dT_{config.quartic_coeff}a_{config.quad_coeff}b_{config.const}c_{config.residual_layers}ResLay_{config.loss_factor}LFac").replace(
                     ".", "")
         elif "QuadSin" in config.data_path:
-            save_path = (
-                    project_config.ROOT_DIR + f"experiments/results/TSPM_LSTM_fQuadSinHF_DriftEvalExp_{epoch}Nep_{config.t0}t0_{config.deltaT:.3e}dT_{config.quad_coeff}a_{config.sin_coeff}b_{config.sin_space_scale}c_{config.residual_layers}ResLay_{config.loss_factor}LFac").replace(
-                ".", "")
+            if "_ST_" in config.scoreNet_trained_path:
+                save_path = (
+                        project_config.ROOT_DIR + f"experiments/results/TSPM_LSTM_ST_fQuadSinHF_DriftEvalExp_{epoch}Nep_{config.t0}t0_{config.deltaT:.3e}dT_{config.quad_coeff}a_{config.sin_coeff}b_{config.sin_space_scale}c_{config.residual_layers}ResLay_{config.loss_factor}LFac").replace(
+                    ".", "")
+            else:
+                save_path = (
+                        project_config.ROOT_DIR + f"experiments/results/TSPM_LSTM_fQuadSinHF_DriftEvalExp_{epoch}Nep_{config.t0}t0_{config.deltaT:.3e}dT_{config.quad_coeff}a_{config.sin_coeff}b_{config.sin_space_scale}c_{config.residual_layers}ResLay_{config.loss_factor}LFac").replace(
+                    ".", "")
         print(f"Save path:{save_path}\n")
         assert config.ts_dims == 1
         np.save(save_path + "_muhats.npy", final_vec_mu_hats)
