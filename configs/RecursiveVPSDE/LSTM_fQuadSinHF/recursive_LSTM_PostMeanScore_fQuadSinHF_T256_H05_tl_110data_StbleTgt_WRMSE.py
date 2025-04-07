9b143daa6847f2c47e4594581e1b5d4b84febbbc
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-04-07 17:47:55 +0100

    Pushing due to lost files

--- configs/RecursiveVPSDE/LSTM_fQuadSinHF/recursive_LSTM_PostMeanScore_fQuadSinHF_T256_H05_tl_110data_StbleTgt_WRMSE.py
+++ configs/RecursiveVPSDE/LSTM_fQuadSinHF/recursive_LSTM_PostMeanScore_fQuadSinHF_T256_H05_tl_110data_StbleTgt_WRMSE.py
@@ -32,7 +32,7 @@ def get_config():
     config.max_diff_steps = 10000
     config.train_eps = 1./config.max_diff_steps  # 1000 * max(int(np.log2(config.ts_length) - 1), 1)
     config.end_diff_time = 1.
-    config.save_freq = 2
+    config.save_freq = 4
     config.lr = 1e-3
     config.max_epochs = [60, 100, 150, 300, 960, 1440, 1920, 2920, 6920, 7190, 8190, 9700, 12920]#, 14920, 16920, 18920, 20920, 22920]
     config.ref_batch_size = 512
