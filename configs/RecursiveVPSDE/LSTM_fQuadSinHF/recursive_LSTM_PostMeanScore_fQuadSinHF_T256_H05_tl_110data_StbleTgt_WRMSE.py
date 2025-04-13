b9393de9820500c2c03c84fbbe0bb66266fc5b64
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-04-13 23:11:50 +0100

    Redoing cosine annealing with lower max learning rate

--- configs/RecursiveVPSDE/LSTM_fQuadSinHF/recursive_LSTM_PostMeanScore_fQuadSinHF_T256_H05_tl_110data_StbleTgt_WRMSE.py
+++ configs/RecursiveVPSDE/LSTM_fQuadSinHF/recursive_LSTM_PostMeanScore_fQuadSinHF_T256_H05_tl_110data_StbleTgt_WRMSE.py
@@ -33,12 +33,12 @@ def get_config():
     config.train_eps = 1./config.max_diff_steps  # 1000 * max(int(np.log2(config.ts_length) - 1), 1)
     config.end_diff_time = 1.
     config.save_freq = 2
-    config.lr = 1e-9
+    config.lr = 1e-3
     config.max_epochs = [60, 100, 150, 300, 960, 1440, 1920, 2920, 6920, 7190, 8190, 9700, 12920]
     config.ref_batch_size = 2048
     config.batch_size = 128
     config.chunk_size = 512
-    config.feat_thresh = 1 / 50.
+    config.feat_thresh = 1 / 500.
     config.isfBm = True
     config.isUnitInterval = True
     config.hybrid = True
