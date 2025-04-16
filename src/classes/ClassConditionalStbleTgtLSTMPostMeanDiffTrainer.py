Correcting ewma code and more stable computation of ESS

src/classes/ClassConditionalStbleTgtLSTMPostMeanDiffTrainer.py
@@ -233,16 +233,18 @@ class ConditionalStbleTgtLSTMPostMeanDiffTrainer(nn.Module):
             # noised_z_chunk = noised_z_chunk.to("cpu")
 
             # Apply the mask to zero out values that are not in the desired range.
-            weights_masked_chunk = weights_chunk * mask_chunk  # [chunk, B1*T, D]
-
+            weights_masked_chunk = weights_chunk * mask_chunk  # [chunk_size, B1*T, D]
+            print(weights_masked_chunk.shape, [chunk_size, B1*T, D])
             # --- Aggregate weights and candidate_Z contributions ---
             # Sum over the candidate dimension (dim=1) to get total weights per target element.
             weight_sum_chunk = weights_masked_chunk.sum(dim=1)  # [chunk, 1]
             assert weight_sum_chunk.shape == (chunk_size, 1)
-            #c = 1./torch.max(torch.abs(weights_masked_chunk[:,0]))
-            #stable_targets_masks.append(
-            #    (torch.pow(torch.sum(c*weights_masked_chunk, dim=1), 2) / torch.sum(torch.pow(c*weights_masked_chunk, 2), dim=1)).to("cpu"))
-            stable_targets_masks.append(kahan_sum(x=weights_masked_chunk))
+            c = 1./torch.max(torch.abs(weights_masked_chunk[:,:, 0]))
+            ESS = (torch.pow(torch.sum(c*weights_masked_chunk, dim=1), 2) / torch.sum(torch.pow(c*weights_masked_chunk, 2), dim=1)).to("cpu")
+            stable_targets_masks.append(ESS)
+            #ESS = kahan_sum(x=weights_masked_chunk)
+            #stable_targets_masks.append(ESS)
+            assert (not torch.any(torch.isnan(ESS)))
             weighted_Z_sum_chunk = (weights_masked_chunk * candidate_Z).sum(dim=1)  # [chunk, D]
             assert weighted_Z_sum_chunk.shape == (chunk_size, D)
             # candidate_Z = candidate_Z.to("cpu")