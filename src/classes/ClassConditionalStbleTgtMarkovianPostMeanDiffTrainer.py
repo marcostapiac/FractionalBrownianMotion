251bf3d5c2f12ed538e5932bbb55c855b907d6a0
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-05-22 09:17:27 +0100

    Higher regularisation

--- src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
+++ src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
@@ -764,11 +764,10 @@ class ConditionalStbleTgtMarkovianPostMeanDiffTrainer(nn.Module):
             average_var_loss_per_epoch = float(torch.mean(torch.stack(all_gpus_var_losses), dim=0).cpu().numpy())
             average_mean_loss_per_epoch = float(torch.mean(torch.stack(all_gpus_mean_losses), dim=0).cpu().numpy())
 
-            # (0.4, 0.15) pre __ epochs; (1., 0.3) post __ epochs.
             ratio = (.99 * average_base_loss_per_epoch) / (average_var_loss_per_epoch + 1e-12)
             self.var_loss_reg = min(ratio, 0.005)
 
-            ratio = (2. * average_base_loss_per_epoch) / (average_mean_loss_per_epoch + 1e-12)
+            ratio = (.75 * average_base_loss_per_epoch) / (average_mean_loss_per_epoch + 1e-12)
             self.mean_loss_reg = min(ratio, 0.01)
 
             # NOTE: .compute() cannot be called on only one process since it will wait for other processes
