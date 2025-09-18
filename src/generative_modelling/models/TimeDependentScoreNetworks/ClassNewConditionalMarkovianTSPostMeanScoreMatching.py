b088d01805937072eba6c034987ed9667248c229
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-09-18 17:11:38 +0100

    Pushing new model

--- src/generative_modelling/models/TimeDependentScoreNetworks/ClassNewConditionalMarkovianTSPostMeanScoreMatching.py
+++ src/generative_modelling/models/TimeDependentScoreNetworks/ClassNewConditionalMarkovianTSPostMeanScoreMatching.py
@@ -84,7 +84,7 @@ class ResidualBlock(nn.Module):
         self.diffusion_projection = nn.Linear(diffusion_hidden_size, residual_channels)
 
         self.output_projection = nn.Conv1d(residual_channels, 2 * residual_channels, 1)
-        nn.init.kaiming_normal_(self.output_projection.weight)
+        nn.init.zeros_(self.output_projection.weight)
 
         # Mild channel-wise normalization to reduce gain spikes
         self.gn_in = nn.GroupNorm(1, residual_channels)
