896ba38852a1436f4d9c72ff09f55319fa318f83
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-09-19 17:21:07 +0100

    Large refactoring

--- configs/RecursiveVPSDE/Markovian_20DLorenz/recursive_Markovian_PostMeanScore_20DLorenz_Stable_T256_H05_tl_110data_StbleTgt.py
+++ configs/RecursiveVPSDE/Markovian_20DLorenz/recursive_Markovian_PostMeanScore_20DLorenz_Stable_T256_H05_tl_110data_StbleTgt.py
@@ -34,7 +34,7 @@ def get_config():
     config.lr = 1e-3
     config.max_epochs = [20000]
     config.ref_batch_size = 1024
-    config.batch_size = 128
+    config.batch_size = 64
     config.chunk_size = 256
     config.feat_thresh = 1./100.
     config.isfBm = True
