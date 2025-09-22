510366eb0feaaa01e33459f7b0d0e4e8db0c156f
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-09-22 20:35:37 +0100

    Change pi0 sampling

--- src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
+++ src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
@@ -878,7 +878,7 @@ class ConditionalStbleTgtMarkovianPostMeanDiffTrainer(nn.Module):
                 if ((epoch + 1) % self.save_every == 0) or epoch == 0:
                     if config.ndims <= 2 or ("BiPot" in config.data_path and config.ndims > 1):
                         evalexp_mse = self._domain_rmse(config=config, epoch=epoch + 1)
-                        if evalexp_mse < self.curr_best_evalexp_mse and (epoch + 1) >= 40:
+                        if evalexp_mse < self.curr_best_evalexp_mse and (epoch + 1) >= 10:
                             self._save_model(filepath=model_filename, final_epoch=epoch + 1, save_type="EE")
                             self.curr_best_evalexp_mse = evalexp_mse
                     track_mse = self._tracking_errors(epoch=epoch + 1, config=config)
