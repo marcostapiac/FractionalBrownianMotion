f8fdcc05265bb7f5b8e1155bbec52ef3c2121beb
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-10-19 17:43:34 +0100

    quadsinhf investigation old network, different stable target

--- src/generative_modelling/data_processing.py
+++ src/generative_modelling/data_processing.py
@@ -300,7 +300,7 @@ def train_and_save_recursive_diffusion_model(data: np.ndarray,
         device = torch.device("cpu")
 
     # Preprocess data
-    if isinstance(trainClass, type) and issubclass(trainClass, ConditionalStbleTgtMarkovianPostMeanDiffTrainer):
+    if isinstance(trainClass, type) and (issubclass(trainClass, ConditionalStbleTgtMarkovianPostMeanDiffTrainer) or issubclass(trainClass, ConditionalStbleTgtMarkovianPostMeanDiffTrainerOld)):
         trainLoader = prepare_recursive_scoreModel_data(data=data, batch_size=config.ref_batch_size, config=config)
     else:
         trainLoader = prepare_recursive_scoreModel_data(data=data, batch_size=config.batch_size, config=config)
