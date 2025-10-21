c763e2e7fd70989ef104fed41999c9354dc373c8
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-10-21 18:58:08 +0100

    latest retraining

--- src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
+++ src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
@@ -1055,19 +1055,26 @@ class ConditionalStbleTgtMarkovianPostMeanDiffTrainer(nn.Module):
                     float(torch.mean(torch.tensor(all_losses_per_epoch[self.epochs_run:])).cpu().numpy()), float(
                         self.loss_aggregator.compute().item())))
                 print(f"Current Loss {curr_loss}\n")
-                self.save_every = 5
+                self.save_every = 1
                 if ((epoch + 1) % self.save_every == 0) or epoch == 0:
                     if config.ndims <= 2 or ("BiPot" in config.data_path and config.ndims > 1):
                         evalexp_mse = self._domain_rmse(config=config, epoch=epoch + 1)
                         if evalexp_mse < self.curr_best_evalexp_mse and (epoch + 1) >= 1:
                             self._save_model(filepath=model_filename, final_epoch=epoch + 1, save_type="EE")
                             self.curr_best_evalexp_mse = evalexp_mse
-                    track_mse = self._tracking_errors(epoch=epoch + 1, config=config)
-                    if track_mse < self.curr_best_track_mse and (epoch + 1) >= 1:
-                        self._save_model(filepath=model_filename, final_epoch=epoch + 1, save_type="")
-                        self.curr_best_track_mse = track_mse
-                    self._save_loss(losses=all_losses_per_epoch, learning_rates=learning_rates, filepath=model_filename)
-                    self._save_snapshot(epoch=epoch)
+                    if ((epoch+1)>300 and (epoch + 1) % 5 == 0)or epoch == 0:
+                        track_mse = self._tracking_errors(epoch=epoch + 1, config=config)
+                        if track_mse < self.curr_best_track_mse and (epoch + 1) >= 1:
+                            self._save_model(filepath=model_filename, final_epoch=epoch + 1, save_type="Trk")
+                            self.curr_best_track_mse = track_mse
+                    elif ((epoch+1)<300 and (epoch + 1) % 25 == 0):
+                        track_mse = self._tracking_errors(epoch=epoch + 1, config=config)
+                        if track_mse < self.curr_best_track_mse and (epoch + 1) >= 1:
+                            self._save_model(filepath=model_filename, final_epoch=epoch + 1, save_type="Trk")
+                            self.curr_best_track_mse = track_mse
+                self._save_model(filepath=model_filename, final_epoch=epoch + 1, save_type="")
+                self._save_loss(losses=all_losses_per_epoch, learning_rates=learning_rates, filepath=model_filename)
+                self._save_snapshot(epoch=epoch)
             if type(self.device_id) == int: dist.barrier()
             print(f"TrackMSE, EvalExpMSE: {self.curr_best_track_mse, self.curr_best_evalexp_mse}\n")
 
