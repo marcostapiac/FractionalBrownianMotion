2fd48e966c2bc68d7a3a4dd646a0f557fed070e6
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-10-21 17:53:17 +0100

    latest retraining

--- src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
+++ src/classes/ClassConditionalStbleTgtMarkovianPostMeanDiffTrainer.py
@@ -1055,7 +1055,7 @@ class ConditionalStbleTgtMarkovianPostMeanDiffTrainer(nn.Module):
                     float(torch.mean(torch.tensor(all_losses_per_epoch[self.epochs_run:])).cpu().numpy()), float(
                         self.loss_aggregator.compute().item())))
                 print(f"Current Loss {curr_loss}\n")
-                self.save_every = 1
+                self.save_every = 10
                 if ((epoch + 1) % self.save_every == 0) or epoch == 0:
                     if config.ndims <= 2 or ("BiPot" in config.data_path and config.ndims > 1):
                         evalexp_mse = self._domain_rmse(config=config, epoch=epoch + 1)
