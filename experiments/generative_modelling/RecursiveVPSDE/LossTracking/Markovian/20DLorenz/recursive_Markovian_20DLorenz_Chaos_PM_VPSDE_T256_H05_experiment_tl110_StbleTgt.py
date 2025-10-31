2b36cc990100bccbae42d8acc6c72cdcefaafaf8
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-10-31 14:16:21 +0000

    redoing 20d and 40dlnz with chaos

--- experiments/generative_modelling/RecursiveVPSDE/LossTracking/Markovian/20DLorenz/recursive_Markovian_20DLorenz_Chaos_PM_VPSDE_T256_H05_experiment_tl110_StbleTgt.py
+++ experiments/generative_modelling/RecursiveVPSDE/LossTracking/Markovian/20DLorenz/recursive_Markovian_20DLorenz_Chaos_PM_VPSDE_T256_H05_experiment_tl110_StbleTgt.py
@@ -44,7 +44,6 @@ if __name__ == "__main__":
                 max(1000, min(int(config.tdata_mult * sum(p.numel() for p in scoreModel.parameters() if p.requires_grad) / (
                             config.ts_length - 1)), 10240)))
             training_size = 10240
-            training_size = 10240
             print(training_size)
             try:
                 data = np.load(config.data_path, allow_pickle=True)
