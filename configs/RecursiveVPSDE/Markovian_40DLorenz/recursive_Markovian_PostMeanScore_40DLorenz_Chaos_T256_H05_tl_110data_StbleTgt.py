96f5e1970787e6aeed0044b9d31499269e081789
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-03 19:23:11 +0000

    valuation

--- configs/RecursiveVPSDE/Markovian_40DLorenz/recursive_Markovian_PostMeanScore_40DLorenz_Chaos_T256_H05_tl_110data_StbleTgt.py
+++ configs/RecursiveVPSDE/Markovian_40DLorenz/recursive_Markovian_PostMeanScore_40DLorenz_Chaos_T256_H05_tl_110data_StbleTgt.py
@@ -42,7 +42,7 @@ def get_config():
     config.ref_batch_size = 512
     config.batch_size = 256
     config.chunk_size = 512
-    config.feat_thresh = 1.#1./10.
+    config.feat_thresh = 1./1.#1./10.
     config.isfBm = True
     config.isUnitInterval = True
     config.hybrid = True
