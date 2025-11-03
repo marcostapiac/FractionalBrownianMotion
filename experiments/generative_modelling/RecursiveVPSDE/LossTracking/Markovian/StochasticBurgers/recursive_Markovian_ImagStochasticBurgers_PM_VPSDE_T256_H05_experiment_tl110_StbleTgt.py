f60dd85c8d3983e92c46c046d6b0606bddc63b74
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-03 16:39:31 +0000

    pushing lessdata experiments for score

--- experiments/generative_modelling/RecursiveVPSDE/LossTracking/Markovian/StochasticBurgers/recursive_Markovian_ImagStochasticBurgers_PM_VPSDE_T256_H05_experiment_tl110_StbleTgt.py
+++ experiments/generative_modelling/RecursiveVPSDE/LossTracking/Markovian/StochasticBurgers/recursive_Markovian_ImagStochasticBurgers_PM_VPSDE_T256_H05_experiment_tl110_StbleTgt.py
@@ -44,8 +44,8 @@ if __name__ == "__main__":
             training_size = int(
                 max(1000, min(int(config.tdata_mult * sum(p.numel() for p in scoreModel.parameters() if p.requires_grad) / (
                             config.ts_length - 1)), 10240)))
-            training_size = 10240
-            training_size = 10240
+            training_size = 1024 if config.feat_thresh == 1. else 10240
+            training_size = 1024 if config.feat_thresh == 1. else 10240
             print(training_size)
             try:
                 data = np.load(config.data_path, allow_pickle=True)
