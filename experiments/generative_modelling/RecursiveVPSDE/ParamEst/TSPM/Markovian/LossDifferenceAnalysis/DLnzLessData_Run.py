38bc373e5f113c699908ca56f582b2f4b6882149
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-11 18:11:51 -0500

    eval

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/DLnzLessData_Run.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/DLnzLessData_Run.py
@@ -306,7 +306,7 @@ nad_eval_true_law = {t: np.inf for t in ["8DLnz", "12DLnz", "20DLnz", "40DLnz"]}
 nad_state_eval = {t: np.inf for t in ["8DLnz", "12DLnz", "20DLnz", "40DLnz"]}
 score_state_eval = {t: np.inf for t in ["8DLnz", "12DLnz", "20DLnz", "40DLnz"]}
 
-for config in [lnz_8d_config, lnz_12d_config, lnz_20d_config,lnz_40d_config]:
+for config in [lnz_40d_config, lnz_12d_config, lnz_20d_config,lnz_8d_config]:
     assert config.feat_thresh == 1.
     assert config.forcing_const == 0.75
     root_score_dir = root_dir
