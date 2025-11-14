fca4541381bc514cbd5d1f38365a1c163d8bb94c
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-14 13:26:53 -0500

    doing nadaraya full data

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/8DLnz_CV.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/8DLnz_CV.py
@@ -349,12 +349,15 @@ CVs = np.zeros(len(bws))
 device_id = _get_device()
 for h in tqdm(range(bws.shape[0])):
     N = prevPath_observations.shape[0]
+    N=10
     cvs = []
     for i in range(N):
-        cvs.append(compute_cv_for_bw_per_path(i, bws[h], device=device_id, path_incs=path_incs,
-                                              prevPath_observations=prevPath_observations, deltaT=config.deltaT))
+        cvs.append(compute_cv_for_bw_per_path(i, bws[h], device=device_id, path_incs=path_incs[:10,:],
+                                              prevPath_observations=prevPath_observations[:10,:], deltaT=config.deltaT))
     CVs[h] = np.sum(cvs)
-bw = [bws[np.argmin(CVs).flatten()]]
+    print(CVs)
+bw = np.array([bws[np.argmin(CVs).flatten()]])
+print(bw)
 inv_H = np.diag(np.power(bw, -2))
 norm_const = 1 / np.sqrt((2. * np.pi) ** config.ndims * (1. / np.linalg.det(inv_H)))
 Nn_tile = 2560000
