4192866059d8d2d0187c9845beb1aec6e0e885fc
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-14 18:10:08 -0500

    Ridge GPU

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Ridge/LessData/Ridge_BiPotLessData_GPU.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Ridge/LessData/Ridge_BiPotLessData_GPU.py
@@ -93,7 +93,7 @@ def find_optimal_Ridge_estimator_coeffs(B, Z, KN, LN, M, device_id):
         opt = scipy.optimize.minimize(obj, opt.x)
     lhat = opt.x[0]
     a = torch.atleast_2d(torch.linalg.inv(BTB+lhat*I)@BTZ)
-    assert (np.allclose((a.T@a).cpu().numpy().flatten()[0], const))
+    #assert (np.allclose((a.T@a).cpu().numpy().flatten()[0], const))
     return a
 
 
