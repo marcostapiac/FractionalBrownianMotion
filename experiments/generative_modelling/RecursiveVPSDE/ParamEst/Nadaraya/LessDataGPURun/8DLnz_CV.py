885e8f3570b1f433232453f5642256fd415d34c4
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-14 13:33:35 -0500

    doing nadaraya full data

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/8DLnz_CV.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/8DLnz_CV.py
@@ -355,7 +355,7 @@ for h in tqdm(range(bws.shape[0])):
         cvs.append(compute_cv_for_bw_per_path(i, bws[h], device=device_id, path_incs=path_incs[:10,:],
                                               prevPath_observations=prevPath_observations[:10,:], deltaT=config.deltaT))
     CVs[h] = np.sum(cvs)
-bw = bws[np.argmin(CVs).flatten(),:]
+bw = bws[np.argmin(CVs).flatten(),:].flatten()
 print(bw)
 inv_H = np.diag(np.power(bw, -2))
 print(inv_H)
