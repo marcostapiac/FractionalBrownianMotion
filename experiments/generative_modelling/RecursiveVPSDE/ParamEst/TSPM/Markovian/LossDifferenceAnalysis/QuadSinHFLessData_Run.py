79d4176e9d2cf5302e80021046e7368783a99cb3
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-14 20:02:02 -0500

    ready for 1d bipot all benchmarks

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/QuadSinHFLessData_Run.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/QuadSinHFLessData_Run.py
@@ -650,6 +650,8 @@ for config in [bipot_config]:
         # Ridge Alt
         curr_states = torch.tensor(all_ridge_states[k:k + block_size, :], device=device_id, dtype=torch.float32).T
         curr_states = torch.concatenate([curr_states, torch.zeros((1, 1), device=device_id, dtype=torch.float32)], dim=-1)
+        AN = torch.min(curr_states.flatten()).cpu().numpy()[0] - 0.5
+        BN = -AN + 0.5
         ridge_basis = spline_basis(paths=curr_states, KN=KN, AN=AN, BN=BN, M=M, device_id=device_id)
         ridge_drift_est = construct_Ridge_estimator(coeffs=ridge_coeffs, B=ridge_basis, LN=LN,device_id=device_id).cpu().numpy().flatten().reshape((curr_states.shape[1]-1, config.ndims))
         ridge_drift_est[curr_states[:, :-1].cpu().numpy().flatten() < AN, :] = np.nan
@@ -677,6 +679,8 @@ for config in [bipot_config]:
 
         # Ridge True
         curr_states = torch.concatenate([curr_states.T, torch.zeros((1, 1), device=device_id, dtype=torch.float32)], dim=-1)
+        AN = torch.min(curr_states.flatten()).cpu().numpy()[0] - 0.5
+        BN = -AN + 0.5
         ridge_basis = spline_basis(paths=curr_states, KN=KN, AN=AN, BN=BN, M=M, device_id=device_id)
         ridge_drift_est = construct_Ridge_estimator(coeffs=ridge_coeffs, B=ridge_basis, LN=LN,device_id=device_id).cpu().numpy().flatten().reshape((curr_states.shape[1]-1, config.ndims))
         ridge_drift_est[curr_states[:, :-1].cpu().numpy().flatten() < AN, :] = np.nan
@@ -705,6 +709,8 @@ for config in [bipot_config]:
 
         # Ridge Uniform
         curr_states = torch.concatenate([curr_states.T, torch.zeros((1, 1), device=device_id, dtype=torch.float32)], dim=-1)
+        AN = torch.min(curr_states.flatten()).cpu().numpy()[0] - 0.5
+        BN = -AN + 0.5
         ridge_basis = spline_basis(paths=curr_states, KN=KN, AN=AN, BN=BN, M=M, device_id=device_id)
         ridge_drift_est = construct_Ridge_estimator(coeffs=ridge_coeffs, B=ridge_basis, LN=LN,
                                                     device_id=device_id).cpu().numpy().flatten().reshape(
