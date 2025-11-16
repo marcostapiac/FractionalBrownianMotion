fd2ec2070ea9586e838d33fdc514062be6c55d39
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-16 12:25:38 -0500

    Pushing bug fixx nad

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/BiPot_Nadaraya_Tracking_GPU.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/BiPot_Nadaraya_Tracking_GPU.py
@@ -368,7 +368,7 @@ if __name__ == "__main__":
             Xs = torch.linspace(-1.5, 1.5, all_true_states.shape[0])[:, np.newaxis].to(device)
             all_true_states = true_drift_gpu(prev=Xs, num_paths=all_true_states.shape[0], config=config)[:, 0, :].cpu().numpy()
             #Xs = torch.as_tensor(all_true_states, dtype=torch.float32, device=device).contiguous()
-            for k in tqdm(range(num_dhats)):
+            for k in (range(num_dhats)):
                 is_ss_path_observations = is_path_observations[np.random.choice(is_idxs, size=num_paths, replace=False),
                                           :]
                 is_prevPath_observations = is_ss_path_observations[:, 1:-1]
