854f2016a204c8b0095d1539d240e34f764ed82c
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-17 13:12:40 +0000

    pushing big fiux for nad

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/40DLnz_Nadaraya_Tracking_GPU_SAV.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/40DLnz_Nadaraya_Tracking_GPU_SAV.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
     shape = prevPath_observations.shape
     num_state_paths = 1000
     mses = {bw_idx: np.inf for bw_idx in (range(0, bws.shape[0]))}
-    same_rndm_idxs = np.arange(num_state_paths)
+    same_rndm_idxs = np.random.choice(np.arange(num_state_paths), 100)
     for bw_idx in tqdm(range(0,bws.shape[0])):
         set_runtime_global(idx=bw_idx)
         bw = bws[bw_idx, :]
