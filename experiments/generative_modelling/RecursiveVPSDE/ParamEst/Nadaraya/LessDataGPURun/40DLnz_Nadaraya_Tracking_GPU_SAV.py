3364ed15f360653243962ac57b4376f9945f3cc9
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-17 13:10:33 +0000

    pushing big fiux for nad

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/40DLnz_Nadaraya_Tracking_GPU_SAV.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/40DLnz_Nadaraya_Tracking_GPU_SAV.py
@@ -323,8 +323,8 @@ if __name__ == "__main__":
             ".", "")
         print(f"Save path for Track {save_path}\n")
 
-        M_tile = 512
-        Nn_tile = 128000
+        M_tile = 256
+        Nn_tile = 64000
         stable = True
         num_dhats = 1 # No variability given we use same training dataset
         device = _get_device(None)
