495146f4ff7053e535d8f5635547bf1474a36770
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-11-17 14:10:12 +0000

    pushing big fiux for nad

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/40DLnz_Nadaraya_Tracking_GPU_SAV.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/Nadaraya/LessDataGPURun/40DLnz_Nadaraya_Tracking_GPU_SAV.py
@@ -5,7 +5,7 @@ import numpy as np
 from tqdm import tqdm
 
 from configs import project_config
-from configs.RecursiveVPSDE.Markovian_fBiPotDDims_NonSep.recursive_Markovian_PostMeanScore_fBiPot12DimsNS_T256_H05_tl_110data_StbleTgt_FULLDATA import \
+from configs.RecursiveVPSDE.Markovian_fBiPotDDims_NonSep.recursive_Markovian_PostMeanScore_fBiPot12DimsNS_T256_H05_tl_110data_StbleTgt import \
     get_config
 from src.classes.ClassFractionalLorenz96 import FractionalLorenz96
 from utils.resource_logger import ResourceLogger, set_runtime_global
