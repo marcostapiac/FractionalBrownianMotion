12a2efe7d37be8cb33195c1078343c69a81f9781
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-12-04 17:47:58 +0000

    pushing

--- utils/drift_evaluation_functions.py
+++ utils/drift_evaluation_functions.py
@@ -257,7 +257,7 @@ def experiment_MLP_DDims_drifts(config, Xs, good, onlyGauss=False):
     num_taus = 100
     es = 1
     dtype = torch.float32
-    chunk_size = max(Xs.shape[0]//10, 1024)
+    chunk_size = max(Xs.shape[0]//100, 512)
     # inputs
     Xs = torch.as_tensor(Xs, device=device, dtype=dtype)
     Xshape = Xs.shape[0]
