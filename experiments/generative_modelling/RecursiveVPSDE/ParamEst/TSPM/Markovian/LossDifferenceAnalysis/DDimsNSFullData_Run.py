00c65a7f5ea489c1af45a837b82191011a189fb0
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-12-05 18:06:28 +0000

    removing typing

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/DDimsNSFullData_Run.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/DDimsNSFullData_Run.py
@@ -134,10 +134,8 @@ def generate_synthetic_paths(config, device_id, good, inv_H, norm_const, prevPat
             dsm  = experiment_MLP_DDims_drifts(config=config, Xs=true_states[:, i-1, :], good=good)
             dsm = dsm[:, -1, :, :].reshape((dsm.shape[0],dsm.shape[2],dsm.shape[
                                                                                            -1] * 1), order="C").mean(axis=1)
-            print("NONOOS\n\n")
-            print(dsm.reshape(-1, config.ts_dims)[:3, :])
-            print("\n\nDDimsDrift\n\n")
-            print(local_score_mean.reshape(-1, config.ts_dims)[:3, :])
+            print("\n\n=======START=====\n\n")
+            print(np.max(np.abs(local_score_mean.reshape(-1, config.ts_dims)-dsm.reshape(-1, config.ts_dims)), axis=0))
             print("\n\n=======END=====\n\n")
             assert np.allclose(local_score_mean.reshape(-1, config.ts_dims), dsm.reshape(-1, config.ts_dims), rtol=1e-6, atol=1e-6)
             x = torch.as_tensor(nad_states[:, i - 1, :], device=device_id, dtype=torch.float32).contiguous()
