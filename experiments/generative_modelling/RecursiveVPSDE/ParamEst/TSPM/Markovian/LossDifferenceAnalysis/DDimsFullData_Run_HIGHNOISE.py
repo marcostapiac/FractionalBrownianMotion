8a1ea74e5d3c6962de85d568f235e13dbb865887
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-12-08 13:58:06 +0000

    eval

--- experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/DDimsFullData_Run_HIGHNOISE.py
+++ experiments/generative_modelling/RecursiveVPSDE/ParamEst/TSPM/Markovian/LossDifferenceAnalysis/DDimsFullData_Run_HIGHNOISE.py
@@ -394,7 +394,7 @@ for config in [ddims_12d_config, ddims_8d_config]:
     assert bw.shape[0] == config.ndims and len(bw.shape) == 1
     inv_H = np.diag(np.power(bw, -2))
     norm_const = 1 / np.sqrt((2. * np.pi) ** config.ndims * (1. / np.linalg.det(inv_H)))
-    Nn_tile = 256000
+    Nn_tile = 128000
     stable = True
     block_size = 512
 
