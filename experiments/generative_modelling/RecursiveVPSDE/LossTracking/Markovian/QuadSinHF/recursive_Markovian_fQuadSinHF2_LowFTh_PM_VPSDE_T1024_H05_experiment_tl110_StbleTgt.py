0d556aa520e77adbd21b8bbc112b43396dc33f9a
Marcos Tapia Costa <marcostapia4@gmail.com>
2025-12-09 00:27:01 +0000

    less data

--- experiments/generative_modelling/RecursiveVPSDE/LossTracking/Markovian/QuadSinHF/recursive_Markovian_fQuadSinHF2_LowFTh_PM_VPSDE_T1024_H05_experiment_tl110_StbleTgt.py
+++ experiments/generative_modelling/RecursiveVPSDE/LossTracking/Markovian/QuadSinHF/recursive_Markovian_fQuadSinHF2_LowFTh_PM_VPSDE_T1024_H05_experiment_tl110_StbleTgt.py
@@ -27,6 +27,7 @@ if __name__ == "__main__":
         assert (config.tdata_mult == 110)
         assert (config.sin_space_scale == 25.)
         assert (config.feat_thresh == 1./500.)
+        assert config.deltaT == 1/1024
         print(config.scoreNet_trained_path, config.dataSize)
         rng = np.random.default_rng()
         scoreModel = ConditionalMarkovianTSPostMeanScoreMatching(
@@ -45,17 +46,9 @@ if __name__ == "__main__":
                             config.ts_length - 1)), 10240)))
             training_size = 1024 if config.feat_thresh == 1. else 10240
             print(training_size)
-            try:
-                data = np.load(config.data_path, allow_pickle=True)
-                assert (data.shape[0] >= training_size)
-            except (FileNotFoundError, pickle.UnpicklingError, AssertionError) as e:
-                print("Error {}; generating synthetic data\n".format(e))
-                data = generate_fQuadSin(config=config, T=config.ts_length, isUnitInterval=config.isUnitInterval,
-                                         S=training_size,
-                                         H=config.hurst, a=config.quad_coeff, b=config.sin_coeff, c=config.sin_space_scale,
-                                         diff=config.diffusion,
-                                         initial_state=config.initState)
-                np.save(config.data_path, data)
+            assert training_size == 10240
+            data = np.load(config.data_path, allow_pickle=True)
+            assert (data.shape[0] >= training_size)
             data = np.concatenate([data[:, [0]] - config.initState, np.diff(data, axis=1)], axis=1)
             data = np.atleast_3d(data[:training_size, :])
             assert (data.shape == (training_size, config.ts_length, config.ts_dims))
